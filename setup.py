4f67f66773a0c7412f2a85c68b7a009661ed100b
Tom Christie <tom@tomchristie.com>
2017-06-05 14:13:09 +0100

    Add missing dependancies

--- setup.py
+++ setup.py
@@ -40,7 +40,9 @@ setup(
     author_email='tom@tomchristie.com',
     packages=get_packages('uvicorn'),
     install_requires=[
-        'gunicorn'
+        'gunicorn',
+        'httptools',
+        'uvloop'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
