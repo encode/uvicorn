2ba8a406bd1127310ea100371face869af27c2bb
Tom Christie <tom@tomchristie.com>
2017-06-05 11:23:39 +0100

    uvicorn

---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+
+from setuptools import setup
+
+sys.dont_write_bytecode = True
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+version = get_version('uvicorn')
+
+
+setup(
+    name='uvicorn',
+    version=version,
+    url='https://github.com/tomchristie/uvicorn',
+    license='BSD',
+    description='Blazingly fast & beautifully expressive Web APIs',
+    author='Tom Christie',
+    author_email='tom@tomchristie.com',
+    packages=get_packages('uvicorn'),
+    install_requires=[
+        'gunicorn'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
---
+++ uvicorn/__init__.py
@@ -0,0 +1,5 @@
+from uvicorn.worker import UvicornWorker
+
+
+__version__ = '0.0.1'
+__all__ = ['UvicornWorker']
