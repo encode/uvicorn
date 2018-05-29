8b1262374185189cc3dee1524f558545665c005c
erm <jermff@gmail.com>
2018-05-29 22:06:42 +1000

    More cleanup from previous refactor

--- uvicorn/protocols/http2.py
+++ uvicorn/protocols/http2.py
@@ -1,8 +1,6 @@
 import asyncio
 import enum
 import email
-import http
-import httptools
 import time
 from h2.config import H2Configuration
 from h2.connection import H2Connection
@@ -17,29 +15,10 @@ def set_time_and_date():
     global DATE_HEADER
 
     CURRENT_TIME = time.time()
-    DATE_HEADER = b''.join([
-        b'date: ',
-        email.utils.formatdate(CURRENT_TIME, usegmt=True).encode(),
-        b'\r\n'
-    ])
-
-
-def get_status_line(status_code):
-    try:
-        phrase = http.HTTPStatus(status_code).phrase.encode()
-    except ValueError:
-        phrase = b''
-    return b''.join([
-        b'HTTP/1.1 ', str(status_code).encode(), b' ', phrase, b'\r\n'
-    ])
-
+    DATE_HEADER = ('date', email.utils.formatdate(CURRENT_TIME, usegmt=True))
 
 CURRENT_TIME = 0.0
-DATE_HEADER = b''
-SERVER_HEADER = b'server: uvicorn\r\n'
-STATUS_LINE = {
-    status_code: get_status_line(status_code) for status_code in range(100, 600)
-}
+DATE_HEADER = ''
 
 set_time_and_date()
 
@@ -51,12 +30,13 @@ class RequestStreamState(enum.Enum):
 
 
 class RequestStream:
-    def __init__(self, transport, scope, stream, protocol):
+    def __init__(self, transport, scope, stream, h2_connection, protocol):
         self.state = RequestStreamState.STARTED
         self.transport = transport
         self.scope = scope
-        self.protocol = protocol
         self.stream = stream
+        self.h2_connection = h2_connection
+        self.protocol = protocol
         self.content_length = None
         self.receive_queue = asyncio.Queue()
 
@@ -79,24 +59,19 @@ class RequestStream:
             headers = message.get('headers', [])
 
             content = [
-                STATUS_LINE[status],
-                SERVER_HEADER,
+                (':status', str(status)),
+                ('server', 'uvicorn'),
                 DATE_HEADER,
             ]
             for header_name, header_value in headers:
                 header = header_name.lower()
                 if header == b'content-length':
                     self.content_length = int(header_value.decode())
-                content.extend([header_name, b': ', header_value, b'\r\n'])
+                content.append((header_name, header_value))
 
-                self.state = RequestStreamState.SENDING_BODY
+            await self.protocol.send_headers(self.stream.stream_id, content)
 
-            response_headers = [
-                (':status', str(status)),
-                ('server', 'uvicorn'),
-            ]
-
-            await self.protocol.send_headers(self.stream.stream_id, response_headers)
+            self.state = RequestStreamState.SENDING_BODY
 
         elif message_type == 'http.response.body':
             body = message.get('body', b'')
@@ -114,26 +89,25 @@ class RequestStream:
             raise Exception('Unexpected message type "%s"' % message_type)
 
         if self.state == RequestStreamState.CLOSED:
-            self.protocol.conn.end_stream(self.stream.stream_id)
-            self.transport.write(self.protocol.conn.data_to_send())
+            self.h2_connection.end_stream(self.stream.stream_id)
+            self.transport.write(self.h2_connection.data_to_send())
 
 
 class H2Stream:
-    def __init__(self, stream_id, conn, protocol):
+    def __init__(self, transport, h2_connection, stream_id, protocol):
+        self.transport = transport
+        self.h2_connection = h2_connection
         self.stream_id = stream_id
-        self.conn = conn
         self.protocol = protocol
         self.loop = asyncio.get_event_loop()
-        self.transport = protocol.transport
-        self.is_complete = False
 
     @property
     def window_size(self):
-        return self.conn.local_flow_control_window(self.stream_id)
+        return self.h2_connection.local_flow_control_window(self.stream_id)
 
     @property
     def max_frame_size(self):
-        return self.conn.max_outbound_frame_size
+        return self.h2_connection.max_outbound_frame_size
 
     def get_chunk_size(self, data):
         return min(min(self.window_size, len(data)), self.max_frame_size)
@@ -149,8 +123,8 @@ class H2Stream:
             self.write(data)
 
     def write(self, data):
-        self.conn.send_data(self.stream_id, data)
-        self.transport.write(self.conn.data_to_send())
+        self.h2_connection.send_data(self.stream_id, data)
+        self.transport.write(self.h2_connection.data_to_send())
 
     def chunked_write(self, data):
         if not data:
@@ -173,11 +147,10 @@ class H2Protocol(asyncio.Protocol):
     def __init__(self, consumer, loop=None, state=None):
         self.consumer = consumer
         self.loop = loop or asyncio.get_event_loop()
-        self.request_parser = httptools.HttpRequestParser(self)
         self.state = state or {'total_requests': 0}
 
         config = H2Configuration(client_side=False, header_encoding='utf-8')
-        self.conn = H2Connection(config=config)
+        self.h2_connection = H2Connection(config=config)
 
         self.transport = None
         self.server = None
@@ -190,20 +163,20 @@ class H2Protocol(asyncio.Protocol):
         self.transport = transport
         self.server = transport.get_extra_info('sockname')
         self.client = transport.get_extra_info('peername')
-        self.conn.initiate_connection()
-        self.transport.write(self.conn.data_to_send())
+        self.h2_connection.initiate_connection()
+        self.transport.write(self.h2_connection.data_to_send())
 
     def connection_lost(self, exc):
         self.transport = None
 
     def data_received(self, data):
         try:
-            events = self.conn.receive_data(data)
+            events = self.h2_connection.receive_data(data)
         except ProtocolError as e:
-            self.transport.write(self.conn.data_to_send())
+            self.transport.write(self.h2_connection.data_to_send())
             self.transport.close()
         else:
-            self.transport.write(self.conn.data_to_send())
+            self.transport.write(self.h2_connection.data_to_send())
             for event in events:
                 if isinstance(event, RequestReceived):
                     self.request_received(event.headers, event.stream_id)
@@ -211,10 +184,10 @@ class H2Protocol(asyncio.Protocol):
                     self.stream_complete(event.stream_id)
                 elif isinstance(event, ConnectionTerminated):
                     self.transport.close()
-                self.transport.write(self.conn.data_to_send())
+                self.transport.write(self.h2_connection.data_to_send())
 
     def request_received(self, headers, stream_id):
-        stream = H2Stream(stream_id, self.conn, self)
+        stream = H2Stream(self.transport, self.h2_connection, stream_id, self)
         self.stream_data[stream_id] = stream
 
         headers = dict(headers)
@@ -246,7 +219,8 @@ class H2Protocol(asyncio.Protocol):
             self.transport,
             self.scope,
             stream,
-            protocol=self
+            self.h2_connection,
+            self
         )
         asgi_instance = self.consumer(request.scope)
         self.loop.create_task(asgi_instance(request.receive, request.send))
@@ -255,4 +229,4 @@ class H2Protocol(asyncio.Protocol):
         self.state['total_requests'] += 1
 
     async def send_headers(self, stream_id, response_headers):
-        self.conn.send_headers(stream_id, response_headers)
+        self.h2_connection.send_headers(stream_id, response_headers)
