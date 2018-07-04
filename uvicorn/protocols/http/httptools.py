cdc36e2ef4dc0d7d5b66897c502b837bb1a01a12
Chillar Anand <anand21nanda@gmail.com>
2018-07-04 19:32:33 +0530

    Catch invalid http requests

--- uvicorn/protocols/http/httptools.py
+++ uvicorn/protocols/http/httptools.py
@@ -93,6 +93,10 @@ class HttpToolsProtocol(asyncio.Protocol):
     def data_received(self, data):
         try:
             self.parser.feed_data(data)
+        except httptools.parser.errors.HttpParserError:
+            msg = "Invalid HTTP request received."
+            self.logger.warn(msg)
+            self.transport.close()
         except httptools.HttpParserUpgrade:
             websocket_upgrade(self)
 
