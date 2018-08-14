4c62dad785531a101b131e8c7530dd09e6108cc9
Tom Christie <tom@tomchristie.com>
2018-08-14 13:46:11 +0100

    Add CPython and PyPy markers

--- setup.py
+++ setup.py
@@ -79,6 +79,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     entry_points="""
     [console_scripts]
