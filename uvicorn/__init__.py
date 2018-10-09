21494a0e7b4a063793ec1165c9e94b2c09ead2aa
Tom Christie <tom@tomchristie.com>
2018-10-09 16:07:15 +0100

    Ensure ProxyHeadersMiddleware ignores any messages that are not http/websocket

--- uvicorn/__init__.py
+++ uvicorn/__init__.py
@@ -1,4 +1,4 @@
 from uvicorn.main import main, run
 
-__version__ = "0.3.11"
+__version__ = "0.3.12"
 __all__ = ["main", "run"]
--- uvicorn/middleware/proxy_headers.py
+++ uvicorn/middleware/proxy_headers.py
@@ -15,21 +15,22 @@ class ProxyHeadersMiddleware:
         self.num_proxies = num_proxies
 
     def __call__(self, scope):
-        headers = dict(scope["headers"])
+        if scope["type"] in ("http", "websocket"):
+            headers = dict(scope["headers"])
 
-        if b"x-forwarded-proto" in headers:
-            # Determine if the incoming request was http or https based on
-            # the X-Forwarded-Proto header.
-            x_forwarded_proto = headers[b"x-forwarded-proto"].decode("ascii")
-            scope["scheme"] = x_forwarded_proto.strip()
+            if b"x-forwarded-proto" in headers:
+                # Determine if the incoming request was http or https based on
+                # the X-Forwarded-Proto header.
+                x_forwarded_proto = headers[b"x-forwarded-proto"].decode("ascii")
+                scope["scheme"] = x_forwarded_proto.strip()
 
-        if b"x-forwarded-for" in headers:
-            # Determine the client address from the last trusted IP in the
-            # X-Forwarded-For header. We've lost the connecting client's port
-            # information by now, so only include the host.
-            x_forwarded_for = headers[b"x-forwarded-for"].decode("ascii")
-            host = x_forwarded_for.split(",")[-self.num_proxies].strip()
-            port = 0
-            scope["client"] = (host, port)
+            if b"x-forwarded-for" in headers:
+                # Determine the client address from the last trusted IP in the
+                # X-Forwarded-For header. We've lost the connecting client's port
+                # information by now, so only include the host.
+                x_forwarded_for = headers[b"x-forwarded-for"].decode("ascii")
+                host = x_forwarded_for.split(",")[-self.num_proxies].strip()
+                port = 0
+                scope["client"] = (host, port)
 
         return self.app(scope)
