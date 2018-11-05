ae17436a17efdacac65a8bb44a9f2c88c498f238
Tom Christie <tom@tomchristie.com>
2018-11-05 12:32:24 +0000

    Fix path decoding

--- uvicorn/__init__.py
+++ uvicorn/__init__.py
@@ -1,4 +1,4 @@
 from uvicorn.main import main, run
 
-__version__ = "0.3.17"
+__version__ = "0.3.18"
 __all__ = ["main", "run"]
--- uvicorn/protocols/http/httptools_impl.py
+++ uvicorn/protocols/http/httptools_impl.py
@@ -217,6 +217,9 @@ class HttpToolsProtocol(asyncio.Protocol):
     def on_url(self, url):
         method = self.parser.get_method()
         parsed_url = httptools.parse_url(url)
+        path = parsed_url.path.decode("ascii")
+        if '%' in path:
+            path = unquote(path)
         self.url = url
         self.expect_100_continue = False
         self.headers = []
@@ -228,7 +231,7 @@ class HttpToolsProtocol(asyncio.Protocol):
             "scheme": self.scheme,
             "method": method.decode("ascii"),
             "root_path": self.root_path,
-            "path": parsed_url.path.decode("ascii"),
+            "path": path,
             "query_string": parsed_url.query if parsed_url.query else b"",
             "headers": self.headers,
         }
