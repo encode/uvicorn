c44d633a14a89139e55418aa2d65236a3f589ff4
Tom Christie <tom@tomchristie.com>
2019-01-18 10:50:45 +0000

    Merge master

--- tests/protocols/test_http.py
+++ tests/protocols/test_http.py
@@ -242,7 +242,6 @@ def test_chunked_encoding(protocol_cls):
 
 @pytest.mark.parametrize("protocol_cls", [HttpToolsProtocol, H11Protocol])
 def test_chunked_encoding_empty_body(protocol_cls):
-<<<<<<< HEAD
     def app(scope):
         return Response(
             b"Hello, world!", status_code=200, headers={"transfer-encoding": "chunked"}
@@ -252,37 +251,11 @@ def test_chunked_encoding_empty_body(protocol_cls):
     protocol.data_received(SIMPLE_GET_REQUEST)
     protocol.loop.run_one()
     assert b"HTTP/1.1 200 OK" in protocol.transport.buffer
-=======
-    class App():
-        def __init__(self, scope):
-            assert scope['type'] == 'http'
-            self.scope = scope
-
-        async def __call__(self, receive, send):
-            await send({
-                'type': 'http.response.start',
-                'status': 200,
-                'headers': [
-                    [b'content-type', b'text/plain'],
-                ],
-            })
-            await send({
-                'type': 'http.response.body',
-                'body': b'',
-            })
-
-    protocol = get_connected_protocol(App, protocol_cls)
-    protocol.data_received(SIMPLE_GET_REQUEST)
-    protocol.loop.run_one()
-    assert b"HTTP/1.1 200 OK" in protocol.transport.buffer
-    print(protocol.transport.buffer)
->>>>>>> master
     assert protocol.transport.buffer.count(b'0\r\n\r\n') == 1
     assert not protocol.transport.is_closing()
 
 
 @pytest.mark.parametrize("protocol_cls", [HttpToolsProtocol, H11Protocol])
-<<<<<<< HEAD
 def test_chunked_encoding_head_request(protocol_cls):
     def app(scope):
         return Response(
@@ -297,8 +270,6 @@ def test_chunked_encoding_head_request(protocol_cls):
 
 
 @pytest.mark.parametrize("protocol_cls", [HttpToolsProtocol, H11Protocol])
-=======
->>>>>>> master
 def test_pipelined_requests(protocol_cls):
     def app(scope):
         return Response("Hello, world", media_type="text/plain")
