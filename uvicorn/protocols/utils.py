66cbd1f8ade914efcdd06fc26ab5784232cbf296
Yolley <Yolley@users.noreply.github.com>
2019-06-26 16:51:53 +0300

    Support AF_INET6 address family
    This PR should resolve the issue stated here https://github.com/encode/uvicorn/issues/313 . 
    
    As stated here https://docs.python.org/2/library/socket.html#socket.AF_INET6, "For AF_INET6 address family, a four-tuple (host, port, flowinfo, scopeid) is used", so there should be no possible problems with type conversion on row 12 of this PR. So I think, that there is no need for additional tests, already existing should be enough to cover this.

--- uvicorn/protocols/utils.py
+++ uvicorn/protocols/utils.py
@@ -1,15 +1,24 @@
-def get_remote_addr(transport):
-    info = transport.get_extra_info("peername")
-    if info is not None and isinstance(info, (list, tuple)) and len(info) == 2:
+import socket
+
+
+def get_socket_addr(transport, remote):
+    socket_info = transport.get_extra_info("socket")
+    if remote:
+        info = socket_info.getpeername()
+    else:
+        info = socket_info.getsockname()
+    family = socket_info.family
+    if family in (socket.AF_INET, socket.AF_INET6):
         return (str(info[0]), int(info[1]))
     return None
+
+
+def get_remote_addr(transport):
+    return get_socket_addr(transport, True)
 
 
 def get_local_addr(transport):
-    info = transport.get_extra_info("sockname")
-    if info is not None and isinstance(info, (list, tuple)) and len(info) == 2:
-        return (str(info[0]), int(info[1]))
-    return None
+    return get_socket_addr(transport, False)
 
 
 def is_ssl(transport):
