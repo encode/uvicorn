5f0e0a7974a9c2b660cd3a5dc336501bf4d9fb36
Yolley <comingreal@gmail.com>
2019-06-28 14:32:33 +0300

    fixed lines lengths according to pep8

--- tests/protocols/test_utils.py
+++ tests/protocols/test_utils.py
@@ -27,10 +27,14 @@ def test_get_local_addr_with_socket():
     transport = MockTransport({"socket": MockSocket(family=socket.AF_IPX)})
     assert get_local_addr(transport) == None
 
-    transport = MockTransport({"socket": MockSocket(family=socket.AF_INET6, sockname=["::1", 123])})
+    transport = MockTransport(
+        {"socket": MockSocket(family=socket.AF_INET6, sockname=["::1", 123])}
+    )
     assert get_local_addr(transport) == ("::1", 123)
 
-    transport = MockTransport({"socket": MockSocket(family=socket.AF_INET, sockname=["123.45.6.7", 123])})
+    transport = MockTransport(
+        {"socket": MockSocket(family=socket.AF_INET, sockname=["123.45.6.7", 123])}
+    )
     assert get_local_addr(transport) == ("123.45.6.7", 123)
 
 
@@ -38,10 +42,14 @@ def test_get_remote_addr_with_socket():
     transport = MockTransport({"socket": MockSocket(family=socket.AF_IPX)})
     assert get_remote_addr(transport) == None
 
-    transport = MockTransport({"socket": MockSocket(family=socket.AF_INET6, peername=["::1", 123])})
+    transport = MockTransport(
+        {"socket": MockSocket(family=socket.AF_INET6, peername=["::1", 123])}
+    )
     assert get_remote_addr(transport) == ("::1", 123)
 
-    transport = MockTransport({"socket": MockSocket(family=socket.AF_INET, peername=["123.45.6.7", 123])})
+    transport = MockTransport(
+        {"socket": MockSocket(family=socket.AF_INET, peername=["123.45.6.7", 123])}
+    )
     assert get_remote_addr(transport) == ("123.45.6.7", 123)
 
 
