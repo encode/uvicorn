6e1484da73e278733317368bfd0ac8aba64e15d2
Yolley <comingreal@gmail.com>
2019-06-28 14:22:39 +0300

    fixed MockTransport, added tests for test_utils

--- tests/protocols/test_utils.py
+++ tests/protocols/test_utils.py
@@ -1,4 +1,18 @@
+import socket
 from uvicorn.protocols.utils import get_local_addr, get_remote_addr
+
+
+class MockSocket:
+    def __init__(self, family, peername=None, sockname=None):
+        self.peername = peername
+        self.sockname = sockname
+        self.family = family
+
+    def getpeername(self):
+        return self.peername
+
+    def getsockname(self):
+        return self.sockname
 
 
 class MockTransport:
@@ -6,7 +20,29 @@ class MockTransport:
         self.info = info
 
     def get_extra_info(self, info_type):
-        return self.info[info_type]
+        return self.info.get(info_type)
+
+
+def test_get_local_addr_with_socket():
+    transport = MockTransport({"socket": MockSocket(family=socket.AF_IPX)})
+    assert get_local_addr(transport) == None
+
+    transport = MockTransport({"socket": MockSocket(family=socket.AF_INET6, sockname=["::1", 123])})
+    assert get_local_addr(transport) == ("::1", 123)
+
+    transport = MockTransport({"socket": MockSocket(family=socket.AF_INET, sockname=["123.45.6.7", 123])})
+    assert get_local_addr(transport) == ("123.45.6.7", 123)
+
+
+def test_get_remote_addr_with_socket():
+    transport = MockTransport({"socket": MockSocket(family=socket.AF_IPX)})
+    assert get_remote_addr(transport) == None
+
+    transport = MockTransport({"socket": MockSocket(family=socket.AF_INET6, peername=["::1", 123])})
+    assert get_remote_addr(transport) == ("::1", 123)
+
+    transport = MockTransport({"socket": MockSocket(family=socket.AF_INET, peername=["123.45.6.7", 123])})
+    assert get_remote_addr(transport) == ("123.45.6.7", 123)
 
 
 def test_get_local_addr():
