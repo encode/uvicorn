603cac34f421821dadeea83ddf40ea325820f203
Tom Christie <tom@tomchristie.com>
2019-08-19 11:26:50 +0100

    Generalize websocket test case to work for windows case too

--- tests/protocols/test_websocket.py
+++ tests/protocols/test_websocket.py
@@ -91,10 +91,10 @@ def test_invalid_upgrade(protocol_cls):
             pass  # ok, wsproto 0.13
         else:
             assert response.status_code == 400
-            assert response.text in [
-                "Missing Sec-WebSocket-Key header\n",
-                "Missing Sec-WebSocket-Version header",  # websockets
-                "Missing or empty Sec-WebSocket-Key header\n",  # wsproto
+            assert response.text.lower() in [
+                "missing sec-websocket-key header\n",
+                "missing sec-websocket-version header",  # websockets
+                "missing or empty sec-websocket-key header\n",  # wsproto
             ]
 
 
