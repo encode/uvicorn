a3b0e51d1499fce377c4d0607f1a4102a42673d5
abersheeran <me@abersheeran.com>
2019-09-13 17:20:28 +0800

    use IOCP in windows

--- uvicorn/loops/asyncio.py
+++ uvicorn/loops/asyncio.py
@@ -1,6 +1,9 @@
+import sys
 import asyncio
 
 
 def asyncio_setup():
+    if sys.platform == 'win32' and (sys.version_info.major >= 3 and sys.version_info.minor >= 7):
+        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
