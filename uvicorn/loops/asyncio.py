b576379056094d8fa3c042891e9f904ac2c1afa7
abersheeran <me@abersheeran.com>
2019-09-19 15:47:55 +0800

    change iocp in windows

--- uvicorn/loops/asyncio.py
+++ uvicorn/loops/asyncio.py
@@ -1,9 +1,6 @@
-import sys
 import asyncio
 
 
 def asyncio_setup():
-    if sys.platform == 'win32' and (sys.version_info.major >= 3 and sys.version_info.minor >= 7):
-        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
---
+++ uvicorn/loops/iocp.py
@@ -0,0 +1,9 @@
+import sys
+import asyncio
+
+
+def iocp_setup():
+    if sys.version_info.major >= 3 and sys.version_info.minor >= 7:
+        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+    else:
+        asyncio.set_event_loop(asyncio.ProactorEventLoop())
