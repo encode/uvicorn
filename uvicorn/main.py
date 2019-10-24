a09ed8e8bb93ca2b6cb9f9cb169259760ab5b984
Tom Christie <tom@tomchristie.com>
2019-10-24 11:31:19 +0100

    Linting

--- uvicorn/main.py
+++ uvicorn/main.py
@@ -365,7 +365,9 @@ class Server:
             uds_perms = 0o666
             if os.path.exists(config.uds):
                 uds_perms = os.stat(config.uds).st_mode
-            server = await loop.create_unix_server(create_protocol, path=config.uds, ssl=config.ssl)
+            server = await loop.create_unix_server(
+                create_protocol, path=config.uds, ssl=config.ssl
+            )
             os.chmod(config.uds, uds_perms)
             message = "Uvicorn running on unix socket %s (Press CTRL+C to quit)"
             self.logger.info(message % config.uds)
