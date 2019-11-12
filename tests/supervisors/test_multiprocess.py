6a74d0a8dfa31c6251134936e33cd90708b196a2
Tom Christie <tom@tomchristie.com>
2019-11-12 12:28:48 +0000

    Drop non-functioning test case

--- tests/supervisors/test_multiprocess.py
+++ tests/supervisors/test_multiprocess.py
@@ -1,25 +1,11 @@
+import requests
+
 from uvicorn import Config, Server
 from uvicorn.supervisors import Multiprocess
-import requests
 
 
 def run(sockets):
     pass
-
-
-async def app(scope, receive, send):
-    assert scope['type'] == 'http'
-    await send({
-        'type': 'http.response.start',
-        'status': 200,
-        'headers': [
-            [b'content-type', b'text/plain'],
-        ]
-    })
-    await send({
-        'type': 'http.response.body',
-        'body': b'Hello, world!',
-    })
 
 
 def test_multiprocess_run():
@@ -27,14 +13,3 @@ def test_multiprocess_run():
     supervisor = Multiprocess(config, target=run, sockets=[])
     supervisor.should_exit.set()
     supervisor.run()
-
-
-def test_multiprocess_request():
-    config = Config(app=app, workers=2)
-    server = Server(config=config)
-    supervisor = Multiprocess(config, target=server.run, sockets=[])
-    supervisor.startup()
-    #response = requests.get("http://127.0.0.1:8000")
-    #assert response.status_code == 200
-    #assert response.text == 'Hello, world!'
-    supervisor.shutdown()
