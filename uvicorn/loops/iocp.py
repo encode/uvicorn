abd68704ea75cdbac8ffb1b5c59578a17a716a56
euri10 <euri10@users.noreply.github.com>
2019-12-20 12:11:15 +0100

    Iocp drop (#535)
    * Removed iocp
    * Set SelectorEventLoop on windows with python > 3.8

--- uvicorn/loops/iocp.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import sys
-
-
-def iocp_setup():
-    if sys.version_info.major >= 3 and sys.version_info.minor >= 7:
-        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
-    else:
-        asyncio.set_event_loop(asyncio.ProactorEventLoop())
