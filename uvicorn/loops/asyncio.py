9a048b1b41a3c44d3e4ef0d06da54215c7ab7833
euri10 <benoit.barthelet@gmail.com>
2019-12-20 11:00:24 +0100

    Set SelectorEventLoop on windows with python > 3.8

--- uvicorn/loops/asyncio.py
+++ uvicorn/loops/asyncio.py
@@ -1,6 +1,16 @@
 import asyncio
+import platform
+import sys
 
 
 def asyncio_setup():
+
     loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+    if (
+        sys.version_info.major >= 3
+        and sys.version_info.minor >= 8
+        and platform.system() == "Windows"
+    ):
+        asyncio.set_event_loop_policy(asyncio.SelectorEventLoop())
+    else:
+        asyncio.set_event_loop(loop)
