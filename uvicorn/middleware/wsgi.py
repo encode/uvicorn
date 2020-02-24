e2b528c92e129bde60618f588874f416432b8db4
abersheeran <me@abersheeran.com>
2020-02-25 00:55:42 +0800

    fix django header

--- uvicorn/middleware/wsgi.py
+++ uvicorn/middleware/wsgi.py
@@ -120,7 +120,7 @@ class WSGIResponder:
             status_code, _ = status.split(" ", 1)
             status_code = int(status_code)
             headers = [
-                (name.encode("ascii"), value.encode("ascii"))
+                (name.strip().encode("ascii"), value.strip().encode("ascii"))
                 for name, value in response_headers
             ]
             self.send_queue.append(
