6c15e7abb3e4e450de24de6340fd6e171f13d9ec
euri10 <benoit.barthelet@gmail.com>
2020-03-21 17:28:51 +0100

    removed test app

--- toto.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-
-async def app(scope, receive, send):
-    assert scope["type"] == "websocket"
-    event = await receive()
-    if event["type"] == "websocket.connect":
-        await asyncio.sleep(10)
-        await send({"type": "websocket.accept"})
