c1245dd59ad902513981134a887ad5451f5c87ce
euri10 <benoit.barthelet@gmail.com>
2020-04-12 16:10:50 +0200

    Import gunicor inside test to avoid pypy/win travis error

--- tests/test_gunicorn_workers.py
+++ tests/test_gunicorn_workers.py
@@ -3,7 +3,6 @@ import sys
 import time
 from multiprocessing.context import Process
 
-import gunicorn.app.base
 import pytest
 import requests
 
@@ -13,30 +12,31 @@ async def handler_app(scope, receive, send):
     await send({"type": "http.response.body", "body": b"Hello", "more_body": False})
 
 
-class StandaloneApplication(gunicorn.app.base.BaseApplication):
-    def __init__(self, app, options=None):
-        self.options = options or {}
-        self.application = app
-        super().__init__()
-
-    def load_config(self):
-        config = {
-            key: value
-            for key, value in self.options.items()
-            if key in self.cfg.settings and value is not None
-        }
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.application
-
-
 @pytest.mark.skipif(
     sys.platform.startswith("win") or platform.python_implementation() == "PyPy",
     reason="Skipping uds test on Windows",
 )
 def test_gunicorn_uvicorn():
+    import gunicorn.app.base
+
+    class StandaloneApplication(gunicorn.app.base.BaseApplication):
+        def __init__(self, app, options=None):
+            self.options = options or {}
+            self.application = app
+            super().__init__()
+
+        def load_config(self):
+            config = {
+                key: value
+                for key, value in self.options.items()
+                if key in self.cfg.settings and value is not None
+            }
+            for key, value in config.items():
+                self.cfg.set(key.lower(), value)
+
+        def load(self):
+            return self.application
+
     options = {
         "bind": "%s:%s" % ("127.0.0.1", "8000"),
         "workers": 1,
