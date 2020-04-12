e63cef5800bbc255d812a347467b1c7599efcb34
euri10 <benoit.barthelet@gmail.com>
2020-04-12 15:54:08 +0200

    Lint

--- tests/test_gunicorn_workers.py
+++ tests/test_gunicorn_workers.py
@@ -5,22 +5,23 @@ import gunicorn.app.base
 import requests
 
 
-
 async def handler_app(scope, receive, send):
     await send({"type": "http.response.start", "status": 200, "headers": []})
     await send({"type": "http.response.body", "body": b"Hello", "more_body": False})
 
 
 class StandaloneApplication(gunicorn.app.base.BaseApplication):
-
     def __init__(self, app, options=None):
         self.options = options or {}
         self.application = app
         super().__init__()
 
     def load_config(self):
-        config = {key: value for key, value in self.options.items()
-                  if key in self.cfg.settings and value is not None}
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
         for key, value in config.items():
             self.cfg.set(key.lower(), value)
 
@@ -30,11 +31,10 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
 
 def test_gunicorn_uvicorn():
     options = {
-        'bind': '%s:%s' % ('127.0.0.1', '8000'),
-        'workers': 1,
+        "bind": "%s:%s" % ("127.0.0.1", "8000"),
+        "workers": 1,
         "worker_class": "uvicorn.workers.UvicornWorker",
-        "log_level": "debug"
-        
+        "log_level": "debug",
     }
     gunicorn_uvicorn_server = StandaloneApplication(handler_app, options)
     process = Process(target=gunicorn_uvicorn_server.run)
