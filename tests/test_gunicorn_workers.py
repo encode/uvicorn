27ce99f2d6c879bf2b854da86b0ca91c9febd7d9
euri10 <benoit.barthelet@gmail.com>
2020-04-13 09:25:24 +0200

    Trying to use pytest.importskip for travis to avoid local imports in tests

--- tests/test_gunicorn_workers.py
+++ tests/test_gunicorn_workers.py
@@ -6,36 +6,39 @@ from multiprocessing.context import Process
 import pytest
 import requests
 
+gunicorn_app_base = pytest.importorskip("gunicorn.app.base")
+
 
 async def handler_app(scope, receive, send):
     await send({"type": "http.response.start", "status": 200, "headers": []})
     await send({"type": "http.response.body", "body": b"Hello", "more_body": False})
 
 
+class StandaloneApplication(gunicorn_app_base.BaseApplication):
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+
 @pytest.mark.skipif(
     sys.platform.startswith("win") or platform.python_implementation() == "PyPy",
-    reason="Skipping uds test on Windows",
+    reason="Skipping uds test on Windows and PyPy",
 )
 def test_gunicorn_uvicorn():
-    import gunicorn.app.base
-
-    class StandaloneApplication(gunicorn.app.base.BaseApplication):
-        def __init__(self, app, options=None):
-            self.options = options or {}
-            self.application = app
-            super().__init__()
-
-        def load_config(self):
-            config = {
-                key: value
-                for key, value in self.options.items()
-                if key in self.cfg.settings and value is not None
-            }
-            for key, value in config.items():
-                self.cfg.set(key.lower(), value)
-
-        def load(self):
-            return self.application
 
     options = {
         "bind": "%s:%s" % ("127.0.0.1", "8000"),
