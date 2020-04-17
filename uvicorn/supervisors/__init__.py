d93b0158e16fa217afa1dae6f075b063f29ae6fd
euri10 <benoit.barthelet@gmail.com>
2020-04-17 15:02:49 +0200

    Removed watchdogreload.py

--- uvicorn/supervisors/__init__.py
+++ uvicorn/supervisors/__init__.py
@@ -3,10 +3,6 @@ from uvicorn.supervisors.multiprocess import Multiprocess
 try:
     from uvicorn.supervisors.watchgodreload import WatchGodReload as ChangeReload
 except ImportError:
-    try:
-        from uvicorn.supervisors.watchdogreload import WatchdogReload as ChangeReload
-
-    except ImportError:
-        from uvicorn.supervisors.statreload import StatReload as ChangeReload
+    from uvicorn.supervisors.statreload import StatReload as ChangeReload
 
 __all__ = ["Multiprocess", "ChangeReload"]
