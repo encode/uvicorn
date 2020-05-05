456f2c7c070090961adb92587a162e5c71b23d52
euri10 <benoit.barthelet@gmail.com>
2020-05-05 09:08:13 +0200

    Incoroporate other reqs

--- setup.py
+++ setup.py
@@ -34,22 +34,29 @@ def get_packages(package):
     ]
 
 
-env_marker = (
+env_marker_cpython = (
     "sys_platform != 'win32'"
     " and sys_platform != 'cygwin'"
     " and platform_python_implementation != 'PyPy'"
 )
 
+env_marker_win = (
+    "sys_platform == 'win32'"
+)
+
+
 minimal_requirements = [
     "click==7.*",
     "h11>=0.8,<0.10",
-    "h11==0.8.*",
 ]
 
 extra_requirements = [
     "websockets==8.*",
-    "httptools==0.1.* ;" + env_marker,
-    "uvloop>=0.14.0 ;" + env_marker,
+    "httptools==0.1.* ;" + env_marker_cpython,
+    "uvloop>=0.14.0 ;" + env_marker_cpython,
+    "colorama>=0.4.*;" + env_marker_win,
+    "watchgod>=0.6,<0.7",
+    "python-dotenv==0.13.*"
 ]
 
 
