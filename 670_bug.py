faf4e58f49f9f968fa59b3233a9f00682e98fbd0
euri10 <benoit.barthelet@gmail.com>
2020-05-09 10:03:40 +0200

    Removed bugged app and file that triggered it

--- 670_bug.py
+++
@@ -1,7 +0,0 @@
-from multiprocessing import Process
-import uvicorn
-
-myproc = Process(target=uvicorn.run, kwargs={'app':'app:app', 'workers':2})
-myproc.start()
-# # myproc.terminate()
-# myproc.join()
