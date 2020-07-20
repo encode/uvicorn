d94417bf95f8b4dbea306771f0afb422d15a47a2
euri10 <benoit.barthelet@gmail.com>
2020-07-20 19:05:28 +0200

    Try using bash on windows

--- scripts/install.ps1
+++
@@ -1,2 +0,0 @@
-pip install -r requirements_windows.txt
-pip install -e .
--- scripts/test.ps1
+++
@@ -1,5 +0,0 @@
-pytest --ignore venv --cov=uvicorn --cov=tests --cov-report=term-missing
-coverage html
-autoflake --recursive uvicorn tests
-flake8 uvicorn tests --ignore=W503,E203,E501,E731
-black uvicorn tests --check
