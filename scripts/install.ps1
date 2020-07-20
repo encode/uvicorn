2efe2f1aaebc46400e7f06072beda61cc5025ad9
euri10 <benoit.barthelet@gmail.com>
2020-07-20 17:15:23 +0200

    Blind ps1 2nd pass

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,7 +1,3 @@
 REQUIREMENTS="requirements_windows.txt"
-VENV="venv"
-python3 -m venv "$VENV"
-PIP="$VENV\bin\pip"
-
-"$PIP" install -r "$REQUIREMENTS"
-"$PIP" install -e .
+pip install -r "$REQUIREMENTS"
+pip install -e .
