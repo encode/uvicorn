561234e5eafee053196d1a83d6b0a8ff09f25c3e
euri10 <benoit.barthelet@gmail.com>
2020-07-20 19:15:20 +0200

    Live debug

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,8 @@
 # Use the Python executable provided from the `-p` option, or a default.
 [ "$1" = "-p" ] && PYTHON=$2 || PYTHON="python3"
 
+echo $OSTYPE
+
 if [ "$OSTYPE" = "linux-gnu" ]; then
   REQUIREMENTS="requirements.txt"
 elif [ "$OSTYPE" = "win32" ]; then
