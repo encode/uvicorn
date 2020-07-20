0558730c2b50599d02e0fd5ff82344d72852f769
euri10 <benoit.barthelet@gmail.com>
2020-07-20 17:21:46 +0200

    Blind test.ps1 !

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,6 +1,5 @@
-set PREFIX="venv/bin/"
-PYTHONPATH=. ${PREFIX}pytest --ignore venv --cov=uvicorn --cov=tests --cov-report=term-missing ${@}
-${PREFIX}coverage html
-${PREFIX}autoflake --recursive uvicorn tests
-${PREFIX}flake8 uvicorn tests --ignore=W503,E203,E501,E731
-${PREFIX}black uvicorn tests --check
+PYTHONPATH=. pytest --ignore venv --cov=uvicorn --cov=tests --cov-report=term-missing
+coverage html
+autoflake --recursive uvicorn tests
+flake8 uvicorn tests --ignore=W503,E203,E501,E731
+black uvicorn tests --check
