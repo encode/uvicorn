18d20fa941f0a6ae5def60ea41ac7670347a41df
euri10 <benoit.barthelet@gmail.com>
2020-07-20 19:35:41 +0200

    OSTYPE seems to be msys

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,11 +5,9 @@ set euxo -pipefail
 # Use the Python executable provided from the `-p` option, or a default.
 [ "$1" = "-p" ] && PYTHON=$2 || PYTHON="python3"
 
-echo $OSTYPE
-
 if [ "$OSTYPE" = "linux-gnu" ]; then
   REQUIREMENTS="requirements.txt"
-elif [ "$OSTYPE" = "win32" ]; then
+elif [ "$OSTYPE" = "msys" ]; then
   REQUIREMENTS="requirements_windows.txt"
 else
   echo "SHIT"
