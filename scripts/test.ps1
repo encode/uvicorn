91a040282db6a64443393ee710b22fdd96dfbc85
euri10 <benoit.barthelet@gmail.com>
2020-07-20 17:25:27 +0200

    Blind test.ps1 love

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,4 +1,4 @@
-PYTHONPATH=. pytest --ignore venv --cov=uvicorn --cov=tests --cov-report=term-missing
+pytest --ignore venv --cov=uvicorn --cov=tests --cov-report=term-missing
 coverage html
 autoflake --recursive uvicorn tests
 flake8 uvicorn tests --ignore=W503,E203,E501,E731
