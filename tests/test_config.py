938f72ae601eb34b47261a53ea52bc96834a398c
Marco Paolini <mpaolini@users.noreply.github.com>
2020-07-30 01:25:15 +0200

    Fix crash when --interface is wsgi
    This is a regression introduced in
    ae0fd316f03dbef926e40216024dfb934417d48d (#597)

--- tests/test_config.py
+++ tests/test_config.py
@@ -31,6 +31,7 @@ def test_wsgi_app():
 
     assert isinstance(config.loaded_app, WSGIMiddleware)
     assert config.interface == "wsgi"
+    assert config.asgi_version == "2.0"
 
 
 def test_proxy_headers():
