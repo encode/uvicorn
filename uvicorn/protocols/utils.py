4dc4643955b7a7d5de49a754520ac7447582e935
euri10 <benoit.barthelet@gmail.com>
2020-07-29 20:53:25 +0200

    Handle windows case

--- uvicorn/protocols/utils.py
+++ uvicorn/protocols/utils.py
@@ -17,7 +17,7 @@ def get_remote_addr(transport):
 
         if family in (socket.AF_INET, socket.AF_INET6):
             return (str(info[0]), int(info[1]))
-        elif family is socket.AF_UNIX:
+        elif hasattr(socket, "AF_UNIX") and family is socket.AF_UNIX:
             if isinstance(info, tuple):
                 # fd case
                 # <uvloop.PseudoSocket fd=13, family=AddressFamily.AF_UNIX, type=SocketKind.SOCK_STREAM, proto=0, laddr=('127.0.0.1', 8000), raddr=('127.0.0.1', 38634)>
