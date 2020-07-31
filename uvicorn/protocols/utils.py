918722aef95de009fa20de7701286696455f0440
euri10 <euri10@users.noreply.github.com>
2020-07-31 12:56:52 +0200

    Get the request.client under case uvicorn is run with a fd or a unix socket (#729)
    * Revert "Corrected --proxy-headers client ip/host when using a unix socket (#636)"
    
    This reverts commit a796e1d4
    
    * Distinguish case fd/unix socket to return correctly client
    
    * Handle windows case
    
    * Added test for AF_UNIX socket type
    Modified MockSocket peername to pass tuples instead of list because socket.getpeername() and socket.getsockname() return tuples
    
    * Black
    
    * Removed test, black works locally but not in CI....
    
    * Same deal on the server side of things
    
    * Test on AF_UNIX only if it is in socket
    
    * Simpler handling
    
    * Removed debug leftovers

--- uvicorn/protocols/utils.py
+++ uvicorn/protocols/utils.py
@@ -1,10 +1,4 @@
-import socket
 import urllib
-
-if hasattr(socket, "AF_UNIX"):
-    SUPPORTED_SOCKET_FAMILIES = (socket.AF_INET, socket.AF_INET6, socket.AF_UNIX)
-else:
-    SUPPORTED_SOCKET_FAMILIES = (socket.AF_INET, socket.AF_INET6)
 
 
 def get_remote_addr(transport):
@@ -12,18 +6,12 @@ def get_remote_addr(transport):
     if socket_info is not None:
         try:
             info = socket_info.getpeername()
+            return (str(info[0]), int(info[1])) if isinstance(info, tuple) else None
         except OSError:
             # This case appears to inconsistently occur with uvloop
             # bound to a unix domain socket.
-            family = None
-            info = None
-        else:
-            family = socket_info.family
+            return None
 
-        if family in SUPPORTED_SOCKET_FAMILIES:
-            return (str(info[0]), int(info[1]))
-
-        return None
     info = transport.get_extra_info("peername")
     if info is not None and isinstance(info, (list, tuple)) and len(info) == 2:
         return (str(info[0]), int(info[1]))
@@ -34,10 +22,8 @@ def get_local_addr(transport):
     socket_info = transport.get_extra_info("socket")
     if socket_info is not None:
         info = socket_info.getsockname()
-        family = socket_info.family
-        if family in SUPPORTED_SOCKET_FAMILIES:
-            return (str(info[0]), int(info[1]))
-        return None
+
+        return (str(info[0]), int(info[1])) if isinstance(info, tuple) else None
     info = transport.get_extra_info("sockname")
     if info is not None and isinstance(info, (list, tuple)) and len(info) == 2:
         return (str(info[0]), int(info[1]))
