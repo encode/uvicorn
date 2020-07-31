d43d0f82bf6e8596a1a8cb283b9941dd11d9dd86
euri10 <benoit.barthelet@gmail.com>
2020-07-31 11:45:53 +0200

    Simpler handling

--- uvicorn/protocols/utils.py
+++ uvicorn/protocols/utils.py
@@ -1,32 +1,20 @@
-import socket
 import urllib
 
 
 def get_remote_addr(transport):
     socket_info = transport.get_extra_info("socket")
+    peer_info = transport.get_extra_info("peername")
+    print(socket_info)
+    print(peer_info)
     if socket_info is not None:
         try:
             info = socket_info.getpeername()
+            return (str(info[0]), int(info[1])) if isinstance(info, tuple) else None
         except OSError:
             # This case appears to inconsistently occur with uvloop
             # bound to a unix domain socket.
-            family = None
-            info = None
-        else:
-            family = socket_info.family
+            return None
 
-        if family in (socket.AF_INET, socket.AF_INET6):
-            return (str(info[0]), int(info[1]))
-        elif hasattr(socket, "AF_UNIX") and family is socket.AF_UNIX:
-            if isinstance(info, tuple):
-                # fd case
-                # <uvloop.PseudoSocket fd=13, family=AddressFamily.AF_UNIX, type=SocketKind.SOCK_STREAM, proto=0, laddr=('127.0.0.1', 8000), raddr=('127.0.0.1', 38634)>
-                return (str(info[0]), int(info[1]))
-            else:
-                # unix socket case
-                # <uvloop.PseudoSocket fd=21, family=AddressFamily.AF_UNIX, type=SocketKind.SOCK_STREAM, proto=0, laddr=/tmp/gunicorn.sock>
-                return None
-        return None
     info = transport.get_extra_info("peername")
     if info is not None and isinstance(info, (list, tuple)) and len(info) == 2:
         return (str(info[0]), int(info[1]))
@@ -37,19 +25,8 @@ def get_local_addr(transport):
     socket_info = transport.get_extra_info("socket")
     if socket_info is not None:
         info = socket_info.getsockname()
-        family = socket_info.family
-        if family in (socket.AF_INET, socket.AF_INET6):
-            return (str(info[0]), int(info[1]))
-        elif hasattr(socket, "AF_UNIX") and family is socket.AF_UNIX:
-            if isinstance(info, tuple):
-                # fd case
-                # <uvloop.PseudoSocket fd=13, family=AddressFamily.AF_UNIX, type=SocketKind.SOCK_STREAM, proto=0, laddr=('127.0.0.1', 8000), raddr=('127.0.0.1', 38634)>
-                return (str(info[0]), int(info[1]))
-            else:
-                # unix socket case
-                # <uvloop.PseudoSocket fd=21, family=AddressFamily.AF_UNIX, type=SocketKind.SOCK_STREAM, proto=0, laddr=/tmp/gunicorn.sock>
-                return None
-        return None
+
+        return (str(info[0]), int(info[1])) if isinstance(info, tuple) else None
     info = transport.get_extra_info("sockname")
     if info is not None and isinstance(info, (list, tuple)) and len(info) == 2:
         return (str(info[0]), int(info[1]))
