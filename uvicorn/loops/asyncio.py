75f71ddc949b87ac9e0fce72277c77cca0a34c4b
euri10 <benoit.barthelet@gmail.com>
2020-08-02 13:13:00 +0200

    Typed Loops 1

--- uvicorn/loops/asyncio.py
+++ uvicorn/loops/asyncio.py
@@ -2,9 +2,11 @@ import asyncio
 import platform
 import selectors
 import sys
+from asyncio import AbstractEventLoop
 
 
-def asyncio_setup():
+def asyncio_setup() -> None:
+    loop: AbstractEventLoop
     if (
         sys.version_info.major >= 3
         and sys.version_info.minor >= 8
