03091da7b75559a59ffa144c9b6b5167d3f3d11a
euri10 <benoit.barthelet@gmail.com>
2020-08-04 14:48:40 +0200

    Same test, other reloader

--- tests/supervisors/test_watchgodreload.py
+++ tests/supervisors/test_watchgodreload.py
@@ -64,3 +64,28 @@ def test_should_not_reload_when_dot_file_is_changed(tmpdir):
         reloader.shutdown()
     finally:
         os.chdir(working_dir)
+
+
+def test_should_reload_after_exception(tmpdir):
+    update_file = Path(os.path.join(str(tmpdir), "example.py"))
+    update_file.touch()
+
+    working_dir = os.getcwd()
+    os.chdir(str(tmpdir))
+    try:
+        config = Config(app=None, reload=True)
+        reloader = WatchGodReload(config, target=run, sockets=[])
+        reloader.signal_handler(sig=signal.SIGINT, frame=None)
+        reloader.startup()
+
+        assert not reloader.should_restart()
+        time.sleep(1)
+        with open(update_file, "w") as f:
+            f.write("this will certainly raise a syntax error!")
+        time.sleep(1)
+        assert reloader.should_restart()
+
+        reloader.restart()
+        reloader.shutdown()
+    finally:
+        os.chdir(working_dir)
