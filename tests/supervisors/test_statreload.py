bc6b3dfdb37c2e032ea5cdefcd8e11e1595701ae
euri10 <benoit.barthelet@gmail.com>
2020-08-04 14:44:26 +0200

    Attempt at testing a reload on a broken file

--- tests/supervisors/test_statreload.py
+++ tests/supervisors/test_statreload.py
@@ -45,3 +45,28 @@ def test_should_reload(tmpdir):
         reloader.shutdown()
     finally:
         os.chdir(working_dir)
+
+
+def test_should_reload_after_exception(tmpdir):
+    update_file = Path(os.path.join(str(tmpdir), "example.py"))
+    update_file.touch()
+
+    working_dir = os.getcwd()
+    os.chdir(str(tmpdir))
+    try:
+        config = Config(app=None, reload=True)
+        reloader = StatReload(config, target=run, sockets=[])
+        reloader.signal_handler(sig=signal.SIGINT, frame=None)
+        reloader.startup()
+
+        assert not reloader.should_restart()
+        time.sleep(1)
+        with open(update_file, "w") as f:
+            f.write("this will certainly raise a syntax error!")
+        time.sleep(1)
+        assert reloader.should_restart()
+
+        reloader.restart()
+        reloader.shutdown()
+    finally:
+        os.chdir(working_dir)
