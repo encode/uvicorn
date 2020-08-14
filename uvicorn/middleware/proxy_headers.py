6238179f96d5c2e460d6eaaea10688d33e51627f
euri10 <benoit.barthelet@gmail.com>
2020-08-14 10:33:07 +0200

    Fixed proxy_headers.py, not sure mypy doesnt make me do something silly just because it cant get scope has necessarily a "scheme" and "client" key

--- uvicorn/middleware/proxy_headers.py
+++ uvicorn/middleware/proxy_headers.py
@@ -23,26 +23,31 @@ class ProxyHeadersMiddleware:
         self.always_trust = "*" in self.trusted_hosts
 
     async def __call__(self, scope: Scope, receive: Callable, send: Callable) -> None:
-        if scope["type"] in ("http", "websocket"):
-            client_addr = scope.get("client")
-            client_host = client_addr[0] if client_addr else None
+        client_addr = scope.get("client")
+        client_host = client_addr[0] if client_addr else None
 
-            if self.always_trust or client_host in self.trusted_hosts:
-                headers = dict(scope["headers"])
+        if self.always_trust or client_host in self.trusted_hosts:
+            headers = dict(scope["headers"])
 
-                if b"x-forwarded-proto" in headers:
-                    # Determine if the incoming request was http or https based on
-                    # the X-Forwarded-Proto header.
-                    x_forwarded_proto = headers[b"x-forwarded-proto"].decode("ascii")
+            if b"x-forwarded-proto" in headers:
+                # Determine if the incoming request was http or https based on
+                # the X-Forwarded-Proto header.
+                x_forwarded_proto = headers[b"x-forwarded-proto"].decode("ascii")
+                if scope["type"] == "http":
+                    scope["scheme"] = x_forwarded_proto.strip()
+                elif scope["type"] == "websocket":
                     scope["scheme"] = x_forwarded_proto.strip()
 
-                if b"x-forwarded-for" in headers:
-                    # Determine the client address from the last trusted IP in the
-                    # X-Forwarded-For header. We've lost the connecting client's port
-                    # information by now, so only include the host.
-                    x_forwarded_for = headers[b"x-forwarded-for"].decode("ascii")
-                    host = x_forwarded_for.split(",")[-1].strip()
-                    port = 0
+            if b"x-forwarded-for" in headers:
+                # Determine the client address from the last trusted IP in the
+                # X-Forwarded-For header. We've lost the connecting client's port
+                # information by now, so only include the host.
+                x_forwarded_for = headers[b"x-forwarded-for"].decode("ascii")
+                host = x_forwarded_for.split(",")[-1].strip()
+                port = 0
+                if scope["type"] == "http":
+                    scope["client"] = (host, port)
+                elif scope["type"] == "websocket":
                     scope["client"] = (host, port)
 
         return await self.app(scope, receive, send)
