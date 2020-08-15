5de67ba4279e051a72f2164f7c5e28c3d12506d3
euri10 <benoit.barthelet@gmail.com>
2020-08-15 21:24:11 +0200

    Simplified types

--- uvicorn/_types.py
+++ uvicorn/_types.py
@@ -6,82 +6,26 @@ if sys.version_info < (3, 8):
 else:
     from typing import Literal, TypedDict
 
-# SCOPES
 
-
-class ASGIDict(TypedDict):
+class ASGISpecInfo(TypedDict):
     version: str
     spec_version: Optional[Union[Literal["2.0"], Literal["2.1"]]]
 
 
 class LifespanScope(TypedDict):
-    """
-    https://asgi.readthedocs.io/en/latest/specs/lifespan.html#scope
-    """
-
     type: Literal["lifespan"]
-    asgi: ASGIDict
+    asgi: ASGISpecInfo
 
 
-# lifespan messages receive
-class LifespanReceiveStartup(TypedDict):
-    """
-    https://asgi.readthedocs.io/en/latest/specs/lifespan.html#startup-receive-event
-    """
-
-    type: Literal["lifespan.startup"]
+class LifespanReceiveMessage(TypedDict):
+    type: Literal["lifespan.startup", "lifespan.shutdown"]
 
 
-class LifeSpanReceiveShutdown(TypedDict):
-    """
-    https://asgi.readthedocs.io/en/latest/specs/lifespan.html#shutdown-receive-event
-    """
-
-    type: Literal["lifespan.shutdown"]
-
-
-LifespanReceiveMessage = Union[LifespanReceiveStartup, LifeSpanReceiveShutdown]
-
-# lifespan messages send
-
-
-class LifespanSendStartupComplete(TypedDict):
-    """
-    https://asgi.readthedocs.io/en/latest/specs/lifespan.html#startup-complete-send-event
-    """
-
-    type: Literal["lifespan.startup.complete"]
-
-
-class LifespanSendStartupFailed(TypedDict):
-    """
-    https://asgi.readthedocs.io/en/latest/specs/lifespan.html#startup-failed-send-event
-    """
-
-    type: Literal["lifespan.startup.failed"]
+class LifespanSendMessage(TypedDict):
+    type: Literal[
+        "lifespan.startup.complete",
+        "lifespan.startup.failed",
+        "lifespan.shutdown.complete",
+        "lifespan.shutdown.failed",
+    ]
     message: Optional[str]
-
-
-class LifespanSendShutdownComplete(TypedDict):
-    """
-    https://asgi.readthedocs.io/en/latest/specs/lifespan.html#shutdown-complete-send-event
-    """
-
-    type: Literal["lifespan.shutdown.complete"]
-
-
-class LifespanSendShutdownFailed(TypedDict):
-    """
-    https://asgi.readthedocs.io/en/latest/specs/lifespan.html#shutdown-failed-send-event
-    """
-
-    type: Literal["lifespan.shutdown.failed"]
-    message: Optional[str]
-
-
-LifespanSendMessage = Union[
-    LifespanSendStartupComplete,
-    LifespanSendStartupFailed,
-    LifespanSendShutdownComplete,
-    LifespanSendShutdownFailed,
-]
