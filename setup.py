8d12c4d3a48e5460ff41f9f822561bc7e3d2b542
euri10 <benoit.barthelet@gmail.com>
2020-09-29 09:37:11 +0200

    Revert all but h11 and pyton-dotenv

--- setup.py
+++ setup.py
@@ -50,11 +50,11 @@ minimal_requirements = [
 ]
 
 extra_requirements = [
-    "websockets>=8.*",
-    "httptools>=0.1.* ;" + env_marker_cpython,
+    "websockets==8.*",
+    "httptools==0.1.* ;" + env_marker_cpython,
     "uvloop>=0.14.0 ;" + env_marker_cpython,
     "colorama>=0.4.*;" + env_marker_win,
-    "watchgod>=0.6",
+    "watchgod>=0.6,<0.7",
     "python-dotenv>=0.13.*",
     "PyYAML>=5.1",
 ]
