503537477a61a134f5b88907cc56cd4c6486e2e5
euri10 <benoit.barthelet@gmail.com>
2020-10-07 12:28:34 +0200

    Added fixture for encrypted key

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 import trustme
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 
 
 @pytest.fixture
@@ -26,6 +28,20 @@ def tls_ca_certificate_pem_path(tls_certificate_authority):
 def tls_ca_certificate_private_key_path(tls_certificate_authority):
     with tls_certificate_authority.private_key_pem.tempfile() as private_key:
         yield private_key
+
+
+@pytest.fixture
+def tls_ca_certificate_private_key_encrypted_path(tls_certificate_authority):
+    private_key = serialization.load_pem_private_key(
+        tls_certificate_authority.private_key_pem.bytes(), password=None, backend=default_backend()
+    )
+    encrypted_key = private_key.private_bytes(
+        serialization.Encoding.PEM,
+        serialization.PrivateFormat.TraditionalOpenSSL,
+        serialization.BestAvailableEncryption(b"uvicorn password for the win"),
+    )
+    with trustme.Blob(encrypted_key).tempfile() as private_encrypted_key:
+        yield private_encrypted_key
 
 
 @pytest.fixture
