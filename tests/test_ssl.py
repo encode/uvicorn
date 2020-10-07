6f8945e86d97880dfa29b328dea7f17b5301b575
euri10 <benoit.barthelet@gmail.com>
2020-10-07 12:34:17 +0200

    Added test for passing the password

--- tests/test_ssl.py
+++ tests/test_ssl.py
@@ -92,3 +92,39 @@ def test_run_chain(tls_certificate_pem_path):
         response = requests.get("https://127.0.0.1:8000")
     assert response.status_code == 204
     thread.join()
+
+
+@pytest.mark.skipif(
+    sys.platform.startswith("win"), reason="Skipping SSL test on Windows"
+)
+def test_run_password(tls_ca_certificate_pem_path, tls_ca_certificate_private_key_encrypted_path):
+    class App:
+        def __init__(self, scope):
+            if scope["type"] != "http":
+                raise Exception()
+
+        async def __call__(self, receive, send):
+            await send({"type": "http.response.start", "status": 204, "headers": []})
+            await send({"type": "http.response.body", "body": b"", "more_body": False})
+
+    class CustomServer(Server):
+        def install_signal_handlers(self):
+            pass
+
+    config = Config(
+        app=App,
+        loop="asyncio",
+        limit_max_requests=1,
+        ssl_keyfile=tls_ca_certificate_private_key_encrypted_path,
+        ssl_certfile=tls_ca_certificate_pem_path,
+        ssl_password="uvicorn password for the win"
+    )
+    server = CustomServer(config=config)
+    thread = threading.Thread(target=server.run)
+    thread.start()
+    while not server.started:
+        time.sleep(0.01)
+    with no_ssl_verification():
+        response = requests.get("https://127.0.0.1:8000")
+    assert response.status_code == 204
+    thread.join()
