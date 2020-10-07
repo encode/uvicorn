9e7ebfba89df20c7490d06f3d60367f06735ddd2
euri10 <benoit.barthelet@gmail.com>
2020-10-07 10:31:48 +0200

    2nd pass with more precise fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,24 @@ import pytest
 import trustme
 
 
+@pytest.fixture
+def tls_certificate_authority():
+    return trustme.CA()
+
+
+@pytest.fixture
+def tls_certificate(tls_certificate_authority):
+    return tls_certificate_authority.issue_server_cert(
+        'localhost',
+        '127.0.0.1',
+        '::1',
+    )
+
+
 @pytest.fixture(scope="function")
-def certfile_and_keyfile(tmp_path):
-    ca = trustme.CA()
+def certfile_and_keyfile(tls_certificate_authority, tmp_path):
     certfile = str(tmp_path / "cert.pem")
-    ca.cert_pem.write_to_path(certfile)
+    tls_certificate_authority.cert_pem.write_to_path(certfile)
     keyfile = str(tmp_path / "key.pem")
-    ca.private_key_pem.write_to_path(keyfile)
+    tls_certificate_authority.private_key_pem.write_to_path(keyfile)
     return certfile, keyfile
