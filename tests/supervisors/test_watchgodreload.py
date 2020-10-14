1b32f9971ad4080c67e5779d519dec6b60d3ece6
Lucas <git@roang.info>
2020-10-14 15:12:59 +0200

    Fixes watchgod with common prefixes (#817)
    * Fixes watchgod with common prefixes
    
    Use pathlib to recognize common ancestry instead of string compare
    
    * Add tests for watchgodreload
    
    Test for common prefix paths
    Replace os.path with pathlib

--- tests/supervisors/test_watchgodreload.py
+++ tests/supervisors/test_watchgodreload.py
@@ -22,7 +22,7 @@ def test_watchgodreload(
 
 def test_should_reload_when_python_file_is_changed(tmpdir):
     file = "example.py"
-    update_file = Path(os.path.join(str(tmpdir), file))
+    update_file = Path(tmpdir).joinpath(file)
     update_file.touch()
 
     working_dir = os.getcwd()
@@ -46,7 +46,7 @@ def test_should_reload_when_python_file_is_changed(tmpdir):
 
 def test_should_not_reload_when_dot_file_is_changed(tmpdir):
     file = ".dotted"
-    update_file = Path(os.path.join(str(tmpdir), file))
+    update_file = Path(tmpdir).joinpath(file)
     update_file.touch()
 
     working_dir = os.getcwd()
@@ -63,6 +63,45 @@ def test_should_not_reload_when_dot_file_is_changed(tmpdir):
         assert not reloader.should_restart()
 
         reloader.restart()
+        reloader.shutdown()
+    finally:
+        os.chdir(working_dir)
+
+
+def test_should_reload_when_directories_have_same_prefix(tmpdir):
+    file = "example.py"
+    tmpdir_path = Path(tmpdir)
+    app_dir = tmpdir_path.joinpath("app")
+    app_ext_dir = tmpdir_path.joinpath("app_extension")
+    app_file = app_dir.joinpath(file)
+    app_ext_file = app_ext_dir.joinpath(file)
+    app_dir.mkdir()
+    app_ext_dir.mkdir()
+    app_file.touch()
+    app_ext_file.touch()
+
+    working_dir = os.getcwd()
+    os.chdir(str(tmpdir))
+    try:
+        config = Config(
+            app=None, reload=True, reload_dirs=[str(app_dir), str(app_ext_dir)]
+        )
+        reloader = WatchGodReload(config, target=run, sockets=[])
+        reloader.signal_handler(sig=signal.SIGINT, frame=None)
+        reloader.startup()
+
+        assert not reloader.should_restart()
+        time.sleep(0.1)
+        app_file.touch()
+        assert reloader.should_restart()
+
+        reloader.restart()
+
+        assert not reloader.should_restart()
+        time.sleep(0.1)
+        app_ext_file.touch()
+        assert reloader.should_restart()
+
         reloader.shutdown()
     finally:
         os.chdir(working_dir)
