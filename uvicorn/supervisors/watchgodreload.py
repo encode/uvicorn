ff387101c3a03d21bca100250b111670bf77fefc
Lucas Brandstaetter <lucas@brandstaetter.tech>
2020-10-16 19:57:05 +0200

    Update watchgodreload
    * Use should_watch_dir to filter directories
    * Use glob patterns to filter files

--- uvicorn/supervisors/watchgodreload.py
+++ uvicorn/supervisors/watchgodreload.py
@@ -1,6 +1,4 @@
 import logging
-import os
-import re
 from pathlib import Path
 from socket import socket
 from typing import TYPE_CHECKING, Callable, List, Optional
@@ -13,22 +11,58 @@ from uvicorn.supervisors.basereload import BaseReload
 logger = logging.getLogger("uvicorn.error")
 
 if TYPE_CHECKING:
+    import os
+
     DirEntry = os.DirEntry[str]
 
 
 class CustomWatcher(DefaultWatcher):
-    ignore_dotted_file_regex = r"^\/?(?:\w+\/)*(\.\w+)"
-    ignored: List[str] = []
+    def __init__(self, root_path: Path, config: Config):
+        default_includes = ["*.py"]
+        self.includes = [
+            default
+            for default in default_includes
+            if default not in config.reload_excludes
+        ]
+        self.includes.extend(config.reload_includes)
+        self.includes = list(set(self.includes))
 
-    def __init__(self, root_path: str) -> None:
-        for t in self.ignored_file_regexes:
-            self.ignored.append(t)
-        self.ignored.append(self.ignore_dotted_file_regex)
-        self._ignored = tuple(re.compile(r) for r in self.ignored)
-        super().__init__(root_path)
+        default_excludes = [".*", ".py[cod]", ".sw.*", "~*"]
+        self.excludes = [
+            default
+            for default in default_excludes
+            if default not in config.reload_includes
+        ]
+        self.excludes.extend(config.reload_excludes)
+        self.excludes = list(set(self.excludes))
+
+        self.dirs = config.reload_dirs
+        self.dirs_excludes = config.reload_dirs_excludes
+        super().__init__(str(root_path))
 
     def should_watch_file(self, entry: "DirEntry") -> bool:
-        return not any(r.search(entry.name) for r in self._ignored)
+        entry_path = Path(entry)
+        for include_pattern in self.includes:
+            if entry_path.match(include_pattern):
+                for exclude_pattern in self.excludes:
+                    if entry_path.match(exclude_pattern):
+                        return False
+                return True
+
+        return False
+
+    def should_watch_dir(self, entry: "DirEntry") -> bool:
+        entry_path = Path(entry)
+        for directory in self.dirs:
+            if entry_path == directory or directory in entry_path.parents:
+                for excl_directory in self.dirs_excludes:
+                    if (
+                        entry_path == excl_directory
+                        or excl_directory in entry_path.parents
+                    ):
+                        return False
+                return True
+        return False
 
 
 class WatchGodReload(BaseReload):
@@ -41,26 +75,8 @@ class WatchGodReload(BaseReload):
         super().__init__(config, target, sockets)
         self.reloader_name = "watchgod"
         self.watchers = []
-        watch_dirs = {
-            Path(watch_dir).resolve()
-            for watch_dir in self.config.reload_dirs
-            if Path(watch_dir).is_dir()
-        }
-        watch_dirs_set = set(watch_dirs)
-
-        # remove directories that already have a parent watched, so that we don't have
-        # duplicated change events
-        for watch_dir in watch_dirs:
-            for compare_dir in watch_dirs:
-                if compare_dir is watch_dir:
-                    continue
-
-                if compare_dir in watch_dir.parents:
-                    watch_dirs_set.remove(watch_dir)
-
-        self.watch_dir_set = watch_dirs_set
-        for w in watch_dirs_set:
-            self.watchers.append(CustomWatcher(str(w)))
+        for w in config.reload_dirs:
+            self.watchers.append(CustomWatcher(w.resolve(), self.config))
 
     def should_restart(self) -> bool:
         for watcher in self.watchers:
