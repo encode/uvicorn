b541de848cd5a958972fee930f516aa58ad7efc5
euri10 <benoit.barthelet@gmail.com>
2020-11-12 10:47:52 +0100

    Add pure asgi app, cant reproduce

--- 846_quart_race.py
+++ 846_quart_race.py
@@ -1,3 +1,6 @@
+import asyncio
+from typing import Generator, Any, Set
+
 from quart import Quart, request as qrequest
 
 from starlette.applications import Starlette
@@ -20,8 +23,74 @@ async def quart():
     return data
     # return data, 200, {'Connection': 'close'}
 
+
+async def aapp(scope, receive, send):
+    if scope["type"] == "http":
+        asgi_handler = ASGIHTTPConnection()
+        await asgi_handler(receive, send)
+
+
+class ASGIHTTPConnection:
+
+    def __init__(self):
+        self.body = Body()
+
+    async def __call__(self, receive, send):
+        receiver_task = asyncio.ensure_future(self.handle_messages(self.body, receive))
+        handler_task = asyncio.ensure_future(self.handle_request(self.body, send))
+        done, pending = await asyncio.wait(
+            [handler_task, receiver_task], return_when=asyncio.FIRST_COMPLETED
+        )
+        await self._cancel_tasks(pending)
+
+    async def _cancel_tasks(self, tasks: Set[asyncio.Future]) -> None:
+        for task in tasks:
+            task.cancel()
+        await asyncio.gather(*tasks, return_exceptions=True)
+
+    async def handle_messages(self, body, receive) -> None:
+        while True:
+            message = await receive()
+            if message["type"] == "http.request":
+                body.append(message.get("body", b""))
+                if not message.get("more_body", False):
+                    body.set_complete()
+            elif message["type"] == "http.disconnect":
+                return
+
+    async def handle_request(self, body, send) -> None:
+        data = await body
+        await send({
+            'type': 'http.response.start',
+            'status': 200,
+            'headers': [(b'content-length', b"%d" % len(data))],
+        })
+        await send({
+            'type': 'http.response.body',
+            'body': data,
+            'more_body': False,
+        })
+
+
+class Body:
+
+    def __init__(self) -> None:
+        self._data = bytearray()
+        self._complete: asyncio.Event = asyncio.Event()
+
+    def __await__(self) -> Generator[Any, None, Any]:
+        yield from self._complete.wait().__await__()
+        return bytes(self._data)
+
+    def append(self, data: bytes) -> None:
+        self._data.extend(data)
+
+    def set_complete(self) -> None:
+        self._complete.set()
+
 if __name__ == '__main__':
-    uvicorn.run("846_quart_race:qapp", log_level="trace")
+    uvicorn.run("846_quart_race:aapp", log_level="trace")
+    # uvicorn.run("846_quart_race:qapp", log_level="trace")
     # uvicorn.run("846_quart_race:sapp", log_level="trace")
 
 
