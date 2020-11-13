4ecb9610dacde415f9efc1903d6ae4f1841fc539
euri10 <benoit.barthelet@gmail.com>
2020-11-13 08:35:48 +0100

    Add event set in send coroutine

--- uvicorn/protocols/http/httptools_impl.py
+++ uvicorn/protocols/http/httptools_impl.py
@@ -530,6 +530,7 @@ class RequestResponseCycle:
                 if self.expected_content_length != 0:
                     raise RuntimeError("Response content shorter than Content-Length")
                 self.response_complete = True
+                self.message_event.set()
                 if not self.keep_alive:
                     self.transport.close()
                 self.on_response()
