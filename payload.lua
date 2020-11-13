e51ac735ffa8c20b55a3c0b6532b96105b85e593
euri10 <benoit.barthelet@gmail.com>
2020-11-13 08:47:09 +0100

    Removed working files

--- payload.lua
+++
@@ -1,7 +0,0 @@
-wrk.method = "POST"
-wrk.headers["Content-Type"] = "application/json"
-wrk.body = [[
-{"Inputs":[
- {"Text":"They have been given more opportunities to influence the formation and activities of the legislative and executive bodiies.","ModeOverride":"Proactive"}
- ],"RequestVersion":2}
- ]]
