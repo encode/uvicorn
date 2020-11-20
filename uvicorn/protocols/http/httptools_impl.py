91c592620f51ca9eb0a777fde123e58b4a15e3a0
euri10 <benoit.barthelet@gmail.com>
2020-11-20 09:25:42 +0100

    Removed extra kwrgs from access logging to avoid scope > headers leak

--- uvicorn/protocols/http/httptools_impl.py
+++ uvicorn/protocols/http/httptools_impl.py
@@ -462,7 +462,6 @@ class RequestResponseCycle:
                     get_path_with_query_string(self.scope),
                     self.scope["http_version"],
                     status_code,
-                    extra={"status_code": status_code, "scope": self.scope},
                 )
 
             # Write response status line and headers
