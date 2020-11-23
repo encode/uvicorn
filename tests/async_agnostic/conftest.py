57534e605db2ca5b4c84c6299460d4e9001addd6
florimondmanca <florimond.manca@gmail.com>
2020-11-23 21:26:12 +0100

    Windows compat: ensure an event loop is provisioned even if anyio shut it down

--- tests/async_agnostic/conftest.py
+++ tests/async_agnostic/conftest.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Any
 
 import pytest
@@ -11,7 +12,21 @@ import pytest
             ("trio", {"restrict_keyboard_interrupt_to_checkpoints": True}), id="trio"
         ),
         pytest.param(("curio", {}), id="curio"),
-    ]
+    ],
 )
 def anyio_backend(request: Any) -> str:
     return request.param
+
+
+@pytest.fixture(autouse=True)
+def ensure_event_loop():
+    try:
+        asyncio.get_event_loop()
+    except RuntimeError:
+        # We use anyio; its pytest plugin shuts down and unsets the asyncio event loop
+        # after each test cases. For some reason on Windows when the loop is unset
+        # asyncio won't create one again by itself. So we give it a hand.
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+    yield
