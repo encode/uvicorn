13e62be904d55479e448927bcd837a6b5279167b
florimondmanca <florimond.manca@gmail.com>
2020-12-07 23:24:27 +0100

    Add tool to keep CLI usage in `index.md` in sync

---
+++ tools/gen_cli_usage.py
@@ -0,0 +1,45 @@
+import argparse
+import subprocess
+import sys
+
+
+def get_usage_lines() -> list:
+    res = subprocess.run(["uvicorn", "--help"], stdout=subprocess.PIPE)
+    return res.stdout.decode("utf-8").splitlines()
+
+
+def main(check: bool = False) -> None:
+    with open("docs/index.md") as f:
+        content = f.read()
+
+    lines = content.splitlines()
+    marker_lineno = lines.index("<!-- gen_cli_usage:marker -->")
+    assert lines[marker_lineno + 1] == "```"
+    start = marker_lineno + 2
+    end = next(
+        lineno for lineno, line in enumerate(lines[start:], start) if line == "```"
+    )
+    lines = lines[:start] + get_usage_lines() + lines[end:]
+
+    output = "\n".join(lines)
+
+    if check:
+        if content == output:
+            rv = 0
+        else:
+            print(
+                "ERROR: CLI usage in index.md is out of sync. "
+                "Run scripts/lint to fix."
+            )
+            rv = 1
+        sys.exit(rv)
+
+    with open("docs/index.md", "w") as f:
+        f.write(output)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--check", action="store_true")
+    args = parser.parse_args()
+    main(check=args.check)
