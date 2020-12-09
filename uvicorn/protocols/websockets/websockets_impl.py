ce15aa05e5eabf1fe497f0183f7f133ca9051f0e
euri10 <benoit.barthelet@gmail.com>
2020-12-09 19:25:47 +0100

    Fix 204 by using ensure_open that raises the disconnect

--- uvicorn/protocols/websockets/websockets_impl.py
+++ uvicorn/protocols/websockets/websockets_impl.py
@@ -237,6 +237,7 @@ class WebSocketProtocol(websockets.WebSocketServerProtocol):
 
         await self.handshake_completed_event.wait()
         try:
+            await self.ensure_open()
             data = await self.recv()
         except websockets.ConnectionClosed as exc:
             return {"type": "websocket.disconnect", "code": exc.code}
