9120e7f5fa3000aa73b51032f40562c45601585d
likeanaxon <48111466+likeanaxon@users.noreply.github.com>
2020-12-10 23:20:17 +1100

    ==[ Fixed formatting issues ]==

--- uvicorn/subprocess.py
+++ uvicorn/subprocess.py
@@ -9,10 +9,12 @@ import sys
 multiprocessing.allow_connection_pickling()
 spawn = multiprocessing.get_context("spawn")
 
+
 def get_subprocess(config, target, sockets):
     """
     Called in the parent process, to instantiate a new child process instance.
     The child is not yet started at this point.
+
     * config - The Uvicorn configuration instance.
     * target - A callable that accepts a list of sockets. In practice this will
                be the `Server.run()` method.
@@ -28,9 +30,11 @@ def get_subprocess(config, target, sockets):
 
     return spawn.Process(target=subprocess_started, kwargs=kwargs)
 
+
 def subprocess_started(config, target, sockets):
     """
     Called when the child process starts.
+
     * config - The Uvicorn configuration instance.
     * target - A callable that accepts a list of sockets. In practice this will
                be the `Server.run()` method.
