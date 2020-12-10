d45388a82e28b4d8acb1d4386b36aed2be109626
likeanaxon <48111466+likeanaxon@users.noreply.github.com>
2020-12-10 22:54:42 +1100

    ==[ Fix for #877 ]==

--- uvicorn/subprocess.py
+++ uvicorn/subprocess.py
@@ -9,50 +9,40 @@ import sys
 multiprocessing.allow_connection_pickling()
 spawn = multiprocessing.get_context("spawn")
 
-
 def get_subprocess(config, target, sockets):
     """
     Called in the parent process, to instantiate a new child process instance.
     The child is not yet started at this point.
-
     * config - The Uvicorn configuration instance.
     * target - A callable that accepts a list of sockets. In practice this will
                be the `Server.run()` method.
     * sockets - A list of sockets to pass to the server. Sockets are bound once
                 by the parent process, and then passed to the child processes.
     """
-    # We pass across the stdin fileno, and reopen it in the child process.
-    # This is required for some debugging environments.
-    try:
-        stdin_fileno = sys.stdin.fileno()
-    except OSError:
-        stdin_fileno = None
 
     kwargs = {
         "config": config,
         "target": target,
         "sockets": sockets,
-        "stdin_fileno": stdin_fileno,
     }
 
     return spawn.Process(target=subprocess_started, kwargs=kwargs)
 
-
-def subprocess_started(config, target, sockets, stdin_fileno):
+def subprocess_started(config, target, sockets):
     """
     Called when the child process starts.
-
     * config - The Uvicorn configuration instance.
     * target - A callable that accepts a list of sockets. In practice this will
                be the `Server.run()` method.
     * sockets - A list of sockets to pass to the server. Sockets are bound once
                 by the parent process, and then passed to the child processes.
-    * stdin_fileno - The file number of sys.stdin, so that it can be reattached
-                     to the child process.
     """
     # Re-open stdin.
-    if stdin_fileno is not None:
-        sys.stdin = os.fdopen(stdin_fileno)
+    # This is required for some debugging environments.
+    try:
+        sys.stdin = open('/dev/stdin')
+    except:
+        print("WARNING: Failed to open '/dev/stdin', debugging might not work.")
 
     # Logging needs to be setup again for each child.
     config.configure_logging()
