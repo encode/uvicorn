d23268e1c9654c01bb6b425ff80d29a1a6437e7a
Sebastián Ramírez <tiangolo@gmail.com>
2020-12-14 17:59:54 +0100

    🐛 Fix resetting signals in Gunicorn UvicornWorker
    to fix subprocesses that capture output having an incorrect returncode

--- uvicorn/workers.py
+++ uvicorn/workers.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import signal
 
 from gunicorn.workers.base import Worker
 
@@ -62,7 +63,10 @@ class UvicornWorker(Worker):
         super(UvicornWorker, self).init_process()
 
     def init_signals(self):
-        pass
+        # Reset signals so Gunicorn doesn't swallow subprocess return codes
+        # other signals are set up by Server.install_signal_handlers()
+        for s in self.SIGNALS:
+            signal.signal(s, signal.SIG_DFL)
 
     def run(self):
         self.config.app = self.wsgi
