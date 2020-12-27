ce1de17f3e4508bafc024106577563f6af2db9d3
euri10 <benoit.barthelet@gmail.com>
2020-12-27 16:55:05 +0100

    Blacked

--- tests/middleware/test_proxy_headers.py
+++ tests/middleware/test_proxy_headers.py
@@ -38,10 +38,13 @@ async def test_proxy_headers_no_port():
 @pytest.mark.asyncio
 async def test_proxy_headers_invalid_x_forwarded_for():
     async with httpx.AsyncClient(app=app, base_url="http://testserver") as client:
-        headers = Headers({
-            "X-Forwarded-Proto": "https",
-            "X-Forwarded-For": "\xf0\xfd\xfd\xfd, 1.2.3.4",
-        }, encoding="latin-1")
+        headers = Headers(
+            {
+                "X-Forwarded-Proto": "https",
+                "X-Forwarded-For": "\xf0\xfd\xfd\xfd, 1.2.3.4",
+            },
+            encoding="latin-1",
+        )
         response = await client.get("/", headers=headers)
     assert response.status_code == 200
     assert response.text == "Remote: https://1.2.3.4:0"
