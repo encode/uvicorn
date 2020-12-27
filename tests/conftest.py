288a5999f36200b7c6af24c602377c5900f24ad6
euri10 <benoit.barthelet@gmail.com>
2020-12-27 12:12:29 +0100

    Refactored test_main , last test become irrelevant

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,12 +10,12 @@ from uvicorn import Config, Server
 
 
 @asynccontextmanager
-async def run_server(config: Config):
+async def run_server(config: Config, sockets=None):
     server = Server(config=config)
-    cancel_handle = asyncio.ensure_future(server.serve())
+    cancel_handle = asyncio.ensure_future(server.serve(sockets=sockets))
     await asyncio.sleep(0.1)
     try:
-        yield
+        yield server
     finally:
         await server.shutdown()
         cancel_handle.cancel()
