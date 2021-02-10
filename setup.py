393628ce3461dd74bfa12d7aa1f5d08e62e49c97
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-02-10 12:27:18 +0100

    fix: pin uvloop to the lastest Python 3.6 supported version

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ minimal_requirements = [
 extra_requirements = [
     "websockets==8.*",
     "httptools==0.1.* ;" + env_marker_cpython,
-    "uvloop>=0.14.0 ;" + env_marker_cpython,
+    "uvloop==0.14.0;" + env_marker_cpython,
     "colorama>=0.4;" + env_marker_win,
     "watchgod>=0.6",
     "python-dotenv>=0.13",
