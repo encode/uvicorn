ce7a211d10787485d71c06695b56fb1eae00d692
Dima Boger <kotvberloge@gmail.com>
2021-03-13 16:09:24 +0300

    Fix issues after rebasing

--- uvicorn/middleware/proxy_headers.py
+++ uvicorn/middleware/proxy_headers.py
@@ -8,6 +8,7 @@ the connecting client, rather that the connecting proxy.
 
 https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers#Proxies
 """
+from typing import List
 
 
 class ProxyHeadersMiddleware:
