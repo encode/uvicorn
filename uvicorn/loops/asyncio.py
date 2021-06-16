6214bb1e25a6cf97fa17abd82c500ededce3e223
euri10 <benoit.barthelet@gmail.com>
2021-06-16 20:57:25 +0200

    Revert "Using default win loop"
    This reverts commit 40a83bb5

--- uvicorn/loops/asyncio.py
+++ uvicorn/loops/asyncio.py
@@ -7,14 +7,14 @@ import asyncio
 
 def asyncio_setup() -> None:  # pragma: no cover
     loop: asyncio.AbstractEventLoop
-    # if (
-    #     sys.version_info.major >= 3
-    #     and sys.version_info.minor >= 8
-    #     and platform.system() == "Windows"
-    # ):
-    #     selector = selectors.SelectSelector()
-    #     loop = asyncio.SelectorEventLoop(selector)
-    #     asyncio.set_event_loop(loop)
-    # else:
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+    if (
+        sys.version_info.major >= 3
+        and sys.version_info.minor >= 8
+        and platform.system() == "Windows"
+    ):
+        selector = selectors.SelectSelector()
+        loop = asyncio.SelectorEventLoop(selector)
+        asyncio.set_event_loop(loop)
+    else:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
