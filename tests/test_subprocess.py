6ec164f45511765567e2fbe25a358dd708cda472
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-07-18 14:20:33 +0200

    fix tests

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -1,10 +1,15 @@
 from socket import socket
 from typing import List
+from unittest.mock import patch
 
 from asgiref.typing import ASGIReceiveCallable, ASGISendCallable, Scope
 
 from uvicorn.config import Config
-from uvicorn.subprocess import get_subprocess, subprocess_started
+from uvicorn.subprocess import subprocess_started
+
+
+def server_run(sockets: List[socket]):
+    ...
 
 
 async def app(
@@ -21,7 +26,8 @@ def test_subprocess_started() -> None:
     config = Config(app=app)
     config.load()
 
-    def run_sockets(sockets: List[socket]):
-        ...
-
-    subprocess_started(config, run_sockets, [])
+    with patch("tests.test_subprocess.server_run") as mock_run:
+        with patch.object(config, "configure_logging") as mock_config_logging:
+            subprocess_started(config, server_run, [], None)
+            mock_run.assert_called_once()
+            mock_config_logging.assert_called_once()
