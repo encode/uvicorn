3d857ba875e8e99dd3da70f35825f614b6c1d84b
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-07-23 11:37:47 +0200

    📌 Add a CPython marker on httptools

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ minimal_requirements = [
 
 extra_requirements = [
     "websockets>=9.1",
-    "httptools==0.2.*",
+    "httptools==0.2.*; " + env_marker_cpython,
     "uvloop>=0.14.0,!=0.15.0,!=0.15.1; " + env_marker_cpython,
     "colorama>=0.4;" + env_marker_win,
     "watchgod>=0.6",
