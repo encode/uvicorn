3cbd9d9e1ace1895aaa9dd0b72eed8b03f7ae1c6
Thomas Grainger <tagrain@gmail.com>
2021-08-22 09:48:42 +0100

    use a asyncio-backport for asyncio.run and asyncio.get_running_loop

--- setup.py
+++ setup.py
@@ -42,12 +42,14 @@ env_marker_cpython = (
 
 env_marker_win = "sys_platform == 'win32'"
 env_marker_below_38 = "python_version < '3.8'"
+env_marker_below_37 = "python_version < '3.7'"
 
 minimal_requirements = [
     "asgiref>=3.4.0",
     "click>=7.0",
     "h11>=0.8",
     "typing-extensions;" + env_marker_below_38,
+    "asyncio-backport;" + env_marker_below_37,
 ]
 
 
