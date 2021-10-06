c77854121fdc60298f7924405984e7bed996ef54
abersheeran <me@abersheeran.com>
2021-10-07 01:41:54 +0800

    Delete additional dependancy

--- tests/protocols/for_test_sendfile.py
+++ tests/protocols/for_test_sendfile.py
@@ -1,28 +1,20 @@
-from baize.asgi import FileResponse
-
-
-class OnlySendfileFileResponse(FileResponse):
-    def create_send_or_zerocopy(self, scope, send):
-        async def sendfile(
-            file_descriptor: int,
-            offset: int = None,
-            count: int = None,
-            more_body: bool = False,
-        ) -> None:
-            message = {
-                "type": "http.response.zerocopysend",
-                "file": file_descriptor,
-                "more_body": more_body,
-            }
-            if offset is not None:
-                message["offset"] = offset
-            if count is not None:
-                message["count"] = count
-            await send(message)
-
-        return sendfile
-
-
 async def app(scope, receive, send):
-    response = FileResponse("./README.md")
-    await response(scope, receive, send)
+    with open("./README.md", "rb") as file:
+        content_length = len(file.read())
+        file.seek(0, 0)
+        await send(
+            {
+                "type": "http.response.start",
+                "status": 200,
+                "headers": [
+                    (b"Content-Length", str(content_length).encode("ascii")),
+                    (b"Content-Type", b"text/plain; charset=utf8"),
+                ],
+            }
+        )
+        await send(
+            {
+                "type": "http.response.zerocopysend",
+                "file": file.fileno(),
+            }
+        )
