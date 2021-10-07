1f457b958318aa5b5fe15b0afd3f6e1f85efce89
abersheeran <me@abersheeran.com>
2021-10-08 00:43:15 +0800

    Fixed python version judge

--- uvicorn/protocols/http/sendfile.py
+++ uvicorn/protocols/http/sendfile.py
@@ -1,12 +1,16 @@
 import asyncio
 import os
+import sys
 
 
 def can_sendfile(loop) -> bool:
     """
     Judge loop.sendfile available
     """
-    return (
-        hasattr(asyncio, "ProactorEventLoop")
-        and isinstance(loop, asyncio.ProactorEventLoop)
-    ) or (isinstance(loop, asyncio.SelectorEventLoop) and hasattr(os, "sendfile"))
+    return sys.version_info[:2] >= (3, 7) and (
+        (
+            hasattr(asyncio, "ProactorEventLoop")
+            and isinstance(loop, asyncio.ProactorEventLoop)
+        )
+        or (isinstance(loop, asyncio.SelectorEventLoop) and hasattr(os, "sendfile"))
+    )
