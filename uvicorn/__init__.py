efeb62a8d3e67fc1d9cf844d19fc5aa22fb17dfd
Aber <me@abersheeran.com>
2021-11-15 11:01:28 +0800

    Add serve in __init__.py

--- uvicorn/__init__.py
+++ uvicorn/__init__.py
@@ -1,5 +1,5 @@
 from uvicorn.config import Config
-from uvicorn.main import Server, main, run
+from uvicorn.main import Server, main, run, serve
 
 __version__ = "0.15.0"
-__all__ = ["main", "run", "Config", "Server"]
+__all__ = ["main", "run", "serve", "Config", "Server"]
