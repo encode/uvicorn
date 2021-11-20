8b86815a441a4a664a8f409005463c29912526ee
jammer87 <71280324+jammer87@users.noreply.github.com>
2021-11-20 16:46:23 -0600

    shortened warning

--- uvicorn/loops/asyncio.py
+++ uvicorn/loops/asyncio.py
@@ -7,6 +7,5 @@ logger = logging.getLogger("uvicorn.error")
 
 def asyncio_setup(reload: bool = False, **_) -> None:  # pragma: no cover
     if sys.version_info >= (3, 8) and sys.platform == "win32" and reload:
-        logger.warning("Overriding default event loop due to --reload flag.")
-        logger.warning("The --reload flag should not be used in production on Windows")
+        logger.warning("The --reload flag should not be used in production on Windows.")
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
