2d3213c0672e9b1ab2422de7a44e106cea29ecbc
guyskk <guyskk@qq.com>
2021-12-11 18:17:40 +0800

    fix code style issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,10 +7,9 @@ from cryptography.hazmat.primitives import serialization
 
 from uvicorn.config import LOGGING_CONFIG
 
-
 # Workaround: pytest caplog not able to capture no-propagate loggers
 # See also: https://github.com/pytest-dev/pytest/issues/3697
-LOGGING_CONFIG['loggers']['uvicorn']['propagate'] = True
+LOGGING_CONFIG["loggers"]["uvicorn"]["propagate"] = True
 
 
 @pytest.fixture
