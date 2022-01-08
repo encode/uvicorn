df8b49f6779b9de508416ad3823bac868bb8efcf
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-01-08 17:54:14 +0100

    Add missing http version on websockets scope (#1309)

--- uvicorn/protocols/websockets/websockets_impl.py
+++ uvicorn/protocols/websockets/websockets_impl.py
@@ -133,6 +133,7 @@ class WebSocketProtocol(websockets.WebSocketServerProtocol):
         self.scope = {
             "type": "websocket",
             "asgi": {"version": self.config.asgi_version, "spec_version": "2.1"},
+            "http_version": "1.1",
             "scheme": self.scheme,
             "server": self.server,
             "client": self.client,
