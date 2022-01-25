c84979a92a5d1a95fc02a6ef77af016d4064c22a
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-01-25 10:23:42 +0100

    Remove comment about wsproto version (#1310)

--- uvicorn/protocols/websockets/wsproto_impl.py
+++ uvicorn/protocols/websockets/wsproto_impl.py
@@ -13,7 +13,6 @@ from wsproto.utilities import RemoteProtocolError
 from uvicorn.logging import TRACE_LOG_LEVEL
 from uvicorn.protocols.utils import get_local_addr, get_remote_addr, is_ssl
 
-# Check wsproto version. We've build against 0.13. We don't know about 0.14 yet.
 assert wsproto.__version__ > "0.13", "Need wsproto version 0.13"
 
 
