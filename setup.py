342ae826102408c144869f0871f9de84d8aa7e4c
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-03-11 09:35:55 +0100

    Bump `httptools` to `>=0.4.0` (#1400)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ minimal_requirements = [
 
 extra_requirements = [
     "websockets>=10.0",
-    "httptools>=0.2.0,<0.4.0",
+    "httptools>=0.4.0",
     "uvloop>=0.14.0,!=0.15.0,!=0.15.1; " + env_marker_cpython,
     "colorama>=0.4;" + env_marker_win,
     "watchgod>=0.6",
