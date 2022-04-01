2c2b65939b19217aa33896df6c5fa1faedc29952
Matthias Binzer <matthias.binzer@de.bosch.com>
2022-04-01 20:37:25 +0200

    Rename subprocess module to avoid clash with stdlib
    Same problem as with #1426 and logging
    
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- uvicorn/supervisors/multiprocess.py
+++ uvicorn/supervisors/multiprocess.py
@@ -10,7 +10,7 @@ from typing import Callable, List, Optional
 import click
 
 from uvicorn.config import Config
-from uvicorn.subprocess import get_subprocess
+from uvicorn._subprocess import get_subprocess
 
 HANDLED_SIGNALS = (
     signal.SIGINT,  # Unix signal 2. Sent by Ctrl+C.
