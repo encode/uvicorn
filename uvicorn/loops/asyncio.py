2c670cccf96e578ef9072743a3b0c60d9dec9e21
smitchell6879 <22923016+smitchell6879@users.noreply.github.com>
2022-04-06 06:18:00 -0400

    Fix OSError

--- uvicorn/loops/asyncio.py
+++ uvicorn/loops/asyncio.py
@@ -6,6 +6,7 @@ logger = logging.getLogger("uvicorn.error")
 
 
 def asyncio_setup(reload: bool = False) -> None:  # pragma: no cover
-    if sys.version_info >= (3, 8) and sys.platform == "win32" and reload:
-        logger.warning("The --reload flag should not be used in production on Windows.")
+    if sys.version_info >= (3, 8) and sys.platform == "win32":
+        if reload:
+            logger.warning("The --reload flag should not be used in production on Windows.")
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
