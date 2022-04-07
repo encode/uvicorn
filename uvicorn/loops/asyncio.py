105d12d1524ee8519360686cf8735bd997948155
smitchell6879 <22923016+smitchell6879@users.noreply.github.com>
2022-04-06 20:33:06 -0400

    Fixed Flake8 Errors

--- uvicorn/loops/asyncio.py
+++ uvicorn/loops/asyncio.py
@@ -8,5 +8,6 @@ logger = logging.getLogger("uvicorn.error")
 def asyncio_setup(reload: bool = False) -> None:  # pragma: no cover
     if sys.version_info >= (3, 8) and sys.platform == "win32":
         if reload:
-            logger.warning("The --reload flag should not be used in production on Windows.")
+            logger.warning("The --reload flag should not be \
+                used in production on Windows.")
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
