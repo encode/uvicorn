5329864d6d160dc0a006d792f9c27cc97489a0e6
euri10 <benoit.barthelet@gmail.com>
2022-06-13 08:39:51 +0200

    Added use_subprocess to be more generic

--- uvicorn/loops/auto.py
+++ uvicorn/loops/auto.py
@@ -1,14 +1,11 @@
-from typing import Optional
-
-
-def auto_loop_setup(reload: bool = False, workers: Optional[int] = None) -> None:
+def auto_loop_setup(use_subprocess: bool = False) -> None:
     try:
         import uvloop  # noqa
     except ImportError:  # pragma: no cover
         from uvicorn.loops.asyncio import asyncio_setup as loop_setup
 
-        loop_setup(reload=reload, workers=workers)
+        loop_setup(use_subprocess=use_subprocess)
     else:  # pragma: no cover
         from uvicorn.loops.uvloop import uvloop_setup
 
-        uvloop_setup(reload=reload, workers=workers)
+        uvloop_setup(use_subprocess=use_subprocess)
