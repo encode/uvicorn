91c13067b2df28c55197443c3740eb6beefb33bd
Humberto Rocha <humrochagf@gmail.com>
2022-08-24 02:22:00 -0400

    Remove cyclic references in HTTP implementations (#1604)
    * Remove parser reference to break the cyclic reference to protocol and let garbage colletor to do it's work
    
    * Unset keepalive after closing transport

--- uvicorn/protocols/http/httptools_impl.py
+++ uvicorn/protocols/http/httptools_impl.py
@@ -137,6 +137,9 @@ class HttpToolsProtocol(asyncio.Protocol):
             self.flow.resume_writing()
         if exc is None:
             self.transport.close()
+            self._unset_keepalive_if_required()
+
+        self.parser = None
 
     def eof_received(self) -> None:
         pass
