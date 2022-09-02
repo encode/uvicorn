23811181563516f47e0a7fdd5c7980b041828f2d
Amin Alaee <mohammadamin.alaee@gmail.com>
2022-09-02 08:36:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
+import sys
+
 from setuptools import setup
+
+sys.stderr.write(
+    """
+===============================
+Unsupported installation method
+===============================
+Uvicorn no longer supports installation with `python setup.py install`.
+Please use `python -m pip install .` instead.
+"""
+)
+sys.exit(1)
+
 
 # The below code will never execute, however GitHub is particularly
 # picky about where it finds Python packaging metadata.
