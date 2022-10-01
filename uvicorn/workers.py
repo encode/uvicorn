b80a6dd37b017ddfdbd93e081f0474eae56cd59c
Irfanuddin <irfanudeen08@gmail.com>
2022-10-01 21:50:46 +0530

    feat: add parameters to workers.py

--- uvicorn/workers.py
+++ uvicorn/workers.py
@@ -39,6 +39,7 @@ class UvicornWorker(Worker):
             "timeout_notify": self.timeout,
             "callback_notify": self.callback_notify,
             "limit_max_requests": self.max_requests,
+            "limit_request_header_size": self.cfg.limit_request_header_size,
             "forwarded_allow_ips": self.cfg.forwarded_allow_ips,
         }
 
