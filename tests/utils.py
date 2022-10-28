b0a07995783d945d312de310b7c8c5996551652f
Irfanuddin <irfanudeen08@gmail.com>
2022-10-28 21:09:49 +0530

    use create task

--- tests/utils.py
+++ tests/utils.py
@@ -9,13 +9,13 @@ from uvicorn import Config, Server
 @asynccontextmanager
 async def run_server(config: Config, sockets=None):
     server = Server(config=config)
-    cancel_handle = asyncio.ensure_future(server.serve(sockets=sockets))
+    task = asyncio.create_task(server.serve(sockets=sockets))
     await asyncio.sleep(0.1)
     try:
         yield server
     finally:
         await server.shutdown()
-        cancel_handle.cancel()
+        task.cancel()
 
 
 @contextmanager
