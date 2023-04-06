bc719b6c6c32a944ebfb9ccbeb15d1bec98d52f8
Amin Alaee <mohammadamin.alaee@gmail.com>
2023-04-06 15:52:04 +0200

    Update tests

--- tests/protocols/test_websocket.py
+++ tests/protocols/test_websocket.py
@@ -584,7 +584,7 @@ async def test_asgi_return_value(
     async with run_server(config):
         with pytest.raises(websockets.exceptions.ConnectionClosed) as exc_info:
             await connect(f"ws://127.0.0.1:{unused_tcp_port}")
-        assert exc_info.value.code == 1000 or 1006
+        assert exc_info.value.code in {1000, 1006}
 
 
 @pytest.mark.anyio
