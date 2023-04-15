e0a9592bd81b1642fa39b85ae751ea771a565db9
chaojie <hi@chaojie.fun>
2023-04-15 09:26:23 +0800

    Make auto_loop_setup function more simple

--- uvicorn/loops/auto.py
+++ uvicorn/loops/auto.py
@@ -3,9 +3,7 @@ def auto_loop_setup(use_subprocess: bool = False) -> None:
         import uvloop  # noqa
     except ImportError:  # pragma: no cover
         from uvicorn.loops.asyncio import asyncio_setup as loop_setup
-
-        loop_setup(use_subprocess=use_subprocess)
     else:  # pragma: no cover
-        from uvicorn.loops.uvloop import uvloop_setup
+        from uvicorn.loops.uvloop import uvloop_setup as loop_setup
 
-        uvloop_setup(use_subprocess=use_subprocess)
+    loop_setup(use_subprocess=use_subprocess)
