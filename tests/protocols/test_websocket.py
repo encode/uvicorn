35699d246b2d69374f4a3fffb2585f1ac7de665c
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-06-09 08:54:23 +0200

    Fix linter

--- tests/protocols/test_websocket.py
+++ tests/protocols/test_websocket.py
@@ -746,7 +746,9 @@ async def test_connection_lost_before_handshake_complete(
         port=unused_tcp_port,
     )
     async with run_server(config):
-        task = asyncio.create_task(websocket_session(f"ws://127.0.0.1:{unused_tcp_port}"))  # noqa: E501
+        task = asyncio.create_task(
+            websocket_session(f"ws://127.0.0.1:{unused_tcp_port}")
+        )
         await asyncio.sleep(0.1)
         send_accept_task.set()
 
