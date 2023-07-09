bcbf4416c321acddbcf49273a668c3a52b646a23
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-07-09 21:13:56 +0200

    Raise `ImportError` on circular import (#2040)
    Co-authored-by: Michael Jones <4087139+mikeedjones@users.noreply.github.com>

--- tests/importer/test_importer.py
+++ tests/importer/test_importer.py
@@ -41,3 +41,13 @@ def test_no_import_needed() -> None:
 
     instance = import_from_string(TemporaryFile)
     assert instance == TemporaryFile
+
+
+def test_circular_import_error() -> None:
+    with pytest.raises(ImportError) as exc_info:
+        import_from_string("tests.importer.circular_import_a:bar")
+    expected = (
+        "cannot import name 'bar' from partially initialized module "
+        "'tests.importer.circular_import_a' (most likely due to a circular import)"
+    )
+    assert expected in str(exc_info.value)
