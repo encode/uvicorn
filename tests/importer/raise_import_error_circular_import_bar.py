4a182f999ff783d97fc2240d2a65f8df1164ef6a
JONEMI19 <michael.jones4@kingfisher.com>
2023-07-09 10:01:06 +0000

    linting fixes

--- tests/importer/raise_import_error_circular_import_bar.py
+++ tests/importer/raise_import_error_circular_import_bar.py
@@ -1,3 +1,4 @@
 # Used by test_importer.py
+from .raise_import_error_circular_import_foo import foo  # noqa
 
 bar = 123
--- tests/importer/raise_import_error_circular_import_foo.py
+++ tests/importer/raise_import_error_circular_import_foo.py
@@ -1,3 +1,4 @@
 # Used by test_importer.py
+from .raise_import_error_circular_import_bar import bar  # noqa
 
 foo = 123
