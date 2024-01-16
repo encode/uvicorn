45792ee2412fcd40133d0f8f9ab5a4b129423b85
Sebastián Ramírez <tiangolo@gmail.com>
2024-01-16 14:22:55 +0100

    Update uvicorn/protocols/http/httptools_impl.py
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>

--- uvicorn/protocols/http/httptools_impl.py
+++ uvicorn/protocols/http/httptools_impl.py
@@ -261,7 +261,7 @@ class HttpToolsProtocol(asyncio.Protocol):
         if "%" in path:
             path = urllib.parse.unquote(path)
         full_path = self.root_path + path
-        full_raw_path = self.root_path.encode() + raw_path
+        full_raw_path = self.root_path.encode("ascii") + raw_path
         self.scope["path"] = full_path
         self.scope["raw_path"] = full_raw_path
         self.scope["query_string"] = parsed_url.query or b""
