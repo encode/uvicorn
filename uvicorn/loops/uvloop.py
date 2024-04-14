849169f438c9e502d6d95b5d11062aa6bd318ddb
Marcelo Trylesinski <marcelotryle@gmail.com>
2024-04-14 22:34:43 +0200

    fix linter

--- uvicorn/loops/uvloop.py
+++ uvicorn/loops/uvloop.py
@@ -6,7 +6,5 @@ from collections.abc import Callable
 import uvloop
 
 
-def uvloop_loop_factory(
-    use_subprocess: bool = False,
-) -> Callable[[], asyncio.AbstractEventLoop]:
+def uvloop_loop_factory(use_subprocess: bool = False) -> Callable[[], asyncio.AbstractEventLoop]:
     return uvloop.new_event_loop
