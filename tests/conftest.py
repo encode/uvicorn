9f05147d128ce479d2b6f25b7b546fd23e191280
Matt Gilene <madgilene@gmail.com>
2024-05-27 12:02:29 -0400

    Run formatting on tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -58,7 +58,9 @@ def tls_certificate(tls_certificate_authority: trustme.CA) -> trustme.LeafCert:
 
 @pytest.fixture
 def tls_client_certificate(request, tls_certificate_authority: trustme.CA) -> trustme.LeafCert:
-    return tls_certificate_authority.issue_cert("client@example.com", common_name=getattr(request, "param", "uvicorn client"))
+    return tls_certificate_authority.issue_cert(
+        "client@example.com", common_name=getattr(request, "param", "uvicorn client")
+    )
 
 
 @pytest.fixture
