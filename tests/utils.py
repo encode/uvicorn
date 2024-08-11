0f513d26f23ab6a30f80f374a7d6c7d95dbb9686
Max Kühn <maxfischer2781@gmail.com>
2024-08-11 09:17:14 +0200

    Remove signal testing order dependency (#2382)
    * test received signal
    
    * improve error feedback, do not shadow earlier failures
    
    * fix import sorting
    
    ---------
    
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>

--- tests/utils.py
+++ tests/utils.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import asyncio
 import os
+import signal
 from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager, contextmanager
 from pathlib import Path
@@ -20,6 +21,18 @@ async def run_server(config: Config, sockets: list[socket] | None = None) -> Asy
     finally:
         await server.shutdown()
         task.cancel()
+
+
+@contextmanager
+def assert_signal(sig: signal.Signals):
+    """Check that a signal was received and handled in a block"""
+    seen: set[int] = set()
+    prev_handler = signal.signal(sig, lambda num, frame: seen.add(num))
+    try:
+        yield
+        assert sig in seen, f"process signal {signal.Signals(sig)!r} was not received or handled"
+    finally:
+        signal.signal(sig, prev_handler)
 
 
 @contextmanager
