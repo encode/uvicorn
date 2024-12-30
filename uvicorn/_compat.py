30b80932e97cccbd393c84e3a9c63b47a11ff595
Thomas Grainger <tagrain@gmail.com>
2024-12-30 08:00:07 +0000

    fix linting

--- uvicorn/_compat.py
+++ uvicorn/_compat.py
@@ -55,8 +55,7 @@ else:
             try:
                 _cancel_all_tasks(loop)
                 loop.run_until_complete(loop.shutdown_asyncgens())
-                if sys.version_info >= (3, 9):
-                    loop.run_until_complete(loop.shutdown_default_executor())
+                loop.run_until_complete(loop.shutdown_default_executor())
             finally:
                 if loop_factory is None:
                     asyncio.set_event_loop(None)
