b6651893690f416eecd0f4b77bbfdb5d19c43fe5
Vizonex <114684698+Vizonex@users.noreply.github.com>
2025-01-08 13:21:57 -0600

    reformat auto.py

--- uvicorn/loops/auto.py
+++ uvicorn/loops/auto.py
@@ -3,14 +3,17 @@ def auto_loop_setup(use_subprocess: bool = False) -> None:
         import uvloop  # noqa
     except ImportError:  # pragma: no cover
         try:
-            import winloop # noqa
-        except ImportError: # pragma: no cover
+            import winloop  # noqa
+        except ImportError:
             from uvicorn.loops.asyncio import asyncio_setup as loop_setup
+
             loop_setup(use_subprocess=use_subprocess)
         else:
             from uvicorn.loops.winloop import winloop_setup as loop_setup
+
             loop_setup(use_subprocess=use_subprocess)
     else:  # pragma: no cover
         from uvicorn.loops.uvloop import uvloop_setup
+
         uvloop_setup(use_subprocess=use_subprocess)
 
