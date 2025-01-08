2acc0d25c317b815081200d44217830017a2bc06
Vizonex <114684698+Vizonex@users.noreply.github.com>
2025-01-08 13:08:01 -0600

    Fix problems for unix operating systems

--- uvicorn/loops/winloop.py
+++ uvicorn/loops/winloop.py
@@ -1,6 +1,8 @@
 import asyncio
+import sys
 
-import winloop
+if sys.platform in ("win32", "cygwin"):
+    import winloop
 
-def winloop_setup(use_subprocess: bool = False) -> None:
-    asyncio.set_event_loop_policy(winloop.EventLoopPolicy())
+    def winloop_setup(use_subprocess: bool = False) -> None:
+        asyncio.set_event_loop_policy(winloop.EventLoopPolicy())
