a95df702b3ada12925c00a124f80f85561b64100
Vizonex <114684698+Vizonex@users.noreply.github.com>
2025-01-08 13:24:45 -0600

    revert

--- uvicorn/loops/winloop.py
+++ uvicorn/loops/winloop.py
@@ -1,13 +1,7 @@
 import asyncio
-import sys
 
-if sys.platform in ("win32", "cygwin"):
-    import winloop
+import winloop
 
-    def winloop_setup(use_subprocess: bool = False) -> None:
-        asyncio.set_event_loop_policy(winloop.EventLoopPolicy())
 
-else:
-    # Noop so that tests do not fail
-    def winloop_setup(use_subprocess:bool = False) -> None:
-        return 
+def winloop_setup(use_subprocess: bool = False) -> None:
+    asyncio.set_event_loop_policy(winloop.EventLoopPolicy())
